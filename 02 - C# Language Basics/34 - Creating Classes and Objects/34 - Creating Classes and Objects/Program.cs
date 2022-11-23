685a31f6b4c777f23dd84159885ba34f9acf488a
Leonardo Pinto <leonardoan.pinto@gmail.com>
2022-11-23 07:43:11 -0300

    add class 34 - Creating Classes and Objects

---
+++ 02 - C# Language Basics/34 - Creating Classes and Objects/34 - Creating Classes and Objects/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Sample
+{
+    static void Main()
+    {
+
+        // reference variables
+        // stored in the stack
+        Customer c1, c2;
+
+        // objects stored in the heap
+        c1 = new Customer();
+        c2 = new Customer();
+
+        c1.customerName = "John Doe";
+
+        System.Console.WriteLine(c1.customerName);
+        System.Console.ReadKey();
+
+    }
+}
---
+++ 02 - C# Language Basics/34 - Creating Classes and Objects/ClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿
+// internal class is accessed only in the same assembly
+// public class is accessed in tihe same or other assemblies
+public class Customer
+{
+    public int customerId;
+    public string customerName;
+}
