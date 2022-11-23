076508fe0ea06bdc696f1f7a8fbdba8c318d255c
Leonardo Pinto <leonardoan.pinto@gmail.com>
2022-11-23 08:41:36 -0300

    add class 5.37 - access modifiers of fields

---
+++ 05 - Fields/37 - Access Modifiers/ClassLibrary1/Class1.cs
@@ -0,0 +1,40 @@
+﻿public class Product
+{
+    // fields
+    public int id;
+    private string name;
+    protected int price;
+    private protected int quantity;
+    internal string manufacturer;
+    protected internal int discount;
+}
+
+// child class at the same assembly
+public class DomesticProduct: Product
+{
+    public void Method1()
+    {
+        id = 1;
+        name = "Dell Notebook";
+        price = 100;
+        quantity = 100;
+        manufacturer = "Microsoft";
+        discount = 5;
+    }
+}
+
+// another class at the same assembly
+public class AnotherClass
+{
+    public void Method()
+    {
+        Product product1;
+        product1 = new Product();
+        product1.id = 1;
+        product1.name = "Dell Notebook";
+        product1.price = 100;
+        product1.quantity = 100;
+        product1.manufacturer = "Microsoft";
+        product1.discount = 3;
+    }
+}
---
+++ 05 - Fields/37 - Access Modifiers/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿class Sample
+{
+    static void Main()
+    {
+
+    }
+}
+
+
+// child class at another assembly
+public class InternationalProduct : Product
+{
+    public void Method1()
+    {
+        id = 5;
+        name = "Dell Notebook";
+        price = 100;
+        quantity = 100;
+        manufacturer = "Microsoft";
+        discount = 2;
+    }
+}
+
+
+// another class at another assembly
+public class AnotherClass2
+{
+    public void Method2() 
+    {
+        Product product = new Product();
+        product.id = 10;
+        product.name = "Dell Notebook";
+        product.price = 100;
+        product.quantity = 100;
+        product.manufacturer = "Microsoft";
+        product.discount = 0;
+    }
+}
+
