d6f905c668ecfcc12712f0b48b0e39123ec88f9b
Leonardo Pinto <leonardoan.pinto@gmail.com>
2022-11-23 15:58:23 -0300

    add class 45 - encapsulation

---
+++ 06 - Methods/MethodsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,58 @@
+﻿public class Product
+{
+    public string name;
+    public int cost;
+    public double tax;
+    public static int totalProducts;
+    public const string manufacturer = "Microsoft";
+    public readonly string dateOfPurchase;
+
+    public Product()
+    {
+        dateOfPurchase = System.DateTime.Now.ToShortDateString();
+    }
+
+    public void SetName(string value)
+    {
+        name = value;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetCost(int value)
+    {
+        cost = value;
+    }
+
+    public int GetCost()
+    {
+        return cost;
+    }
+
+    public void CalculateTax()
+    {
+        if (cost <= 1000)
+        {
+            tax = 0.125;
+        } else
+        {
+            tax = 0.25;
+        }
+    }
+
+    public double GetTax()
+    {
+        return tax;
+    }
+
+    public string GetDateOfPurchase()
+    {
+        return dateOfPurchase;
+    }
+
+
+   
+}
---
+++ 06 - Methods/MethodsExample/MethodsExample/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Product product1;
+        product1= new Product();
+        Product.totalProducts++;
+
+        product1.SetName("Wireless Keyboard");
+        product1.SetCost(80);
+        product1.CalculateTax();
+
+
+        System.Console.WriteLine("Product: " + product1.GetName());
+        System.Console.WriteLine("Cost: " + product1.GetCost());
+        System.Console.WriteLine("Tax: " + product1.GetTax());
+        System.Console.WriteLine("Date of purchase: " + product1.GetDateOfPurchase());
+        System.Console.WriteLine("Total number of products: " + Product.totalProducts);
+
+        System.Console.ReadKey();
+
+
+    }
+}
