b2a04842c75823578ed25eb7d90298e0bd3e1410
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-05 16:51:53 -0500

    add task 11

---
+++ 08 - Constructors/Task11/Task11/ClassLibrary1/Class1.cs
@@ -0,0 +1,64 @@
+﻿public class Question
+{
+    public string questionText;
+    public string optionA;
+    public string optionB;
+    public string optionC;
+    public string optionD;
+    public char correctAnswerLetter;
+    private static char defaultCorrectAnswerLetter = 'X';
+
+    public Question()
+    {
+        questionText = null;
+        optionA = null;
+        optionB = null;
+        optionC = null;
+        optionD = null;
+
+        correctAnswerLetter = Question.defaultCorrectAnswerLetter;
+    }
+
+    public Question(string questionText)
+    {
+        this.questionText = questionText;
+        correctAnswerLetter = defaultCorrectAnswerLetter;
+    }
+
+    public Question(string questionText, string optionA, string optionB, string optionC, string optionD, char correctAnswerLetter)
+    {
+        this.questionText = questionText;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        if (correctAnswerLetter == 'A' || correctAnswerLetter == 'B' || correctAnswerLetter == 'C' || correctAnswerLetter == 'D')
+            this.correctAnswerLetter = correctAnswerLetter;
+        else
+            this.correctAnswerLetter = Question.defaultCorrectAnswerLetter;
+       
+    }
+
+    public bool CheckQuestionOptions()
+    {
+        int validOptionsNumber = 0;
+        if (this.optionA != null)
+        {
+            validOptionsNumber++;
+        }
+        if (this.optionB != null)
+        {
+            validOptionsNumber++;
+        }
+        if (this.optionC != null)
+        {
+            validOptionsNumber++;
+        }
+        if (this.optionD != null)
+        {
+            validOptionsNumber++;
+        }
+
+        return validOptionsNumber >= 2;
+    }
+}
---
+++ 08 - Constructors/Task11/Task11/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        string questionText = "What is the capital of USA?";
+        string optionA = "London";
+        string optionB = "New York";
+        string optionC = "Washington D.C.";
+        string optionD = "San Diego";
+        char correctAnswerText = 'C';
+
+        Question question1 = new Question();
+
+        Question question2 = new Question(questionText);
+        Question question3 = new Question(questionText, optionA, optionB, optionC, optionD, correctAnswerText);
+        Question question4 = new Question() { questionText = questionText, optionA = optionA, optionB = optionB, optionC = optionC, optionD = optionD };
+    }
+}
