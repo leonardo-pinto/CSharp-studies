9a6f360786ab1e6739c3499ee21aff755d4f49c5
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-06 11:50:20 -0500

    add class 83 - creating indexers

---
+++ 09 - Properties and Indexes/Indexers/ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿public class Car
+{
+    // private field
+    private string[] _brands = new string[] { "BMW", "Audi", "Toyota" };
+
+    // public indexer
+    public string this[int index]
+    {
+        set
+        {
+            this._brands[index] = value;
+
+        }
+        get
+        { 
+            return this._brands[index];
+        }
+    }
+
+
+}
---
+++ 09 - Properties and Indexes/Indexers/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Car car = new Car();
+        System.Console.WriteLine(car[0]);
+        car[0] = "Hyundai";
+        System.Console.WriteLine(car[0]);
+
+
+        System.Console.ReadKey();
+    }
+}
