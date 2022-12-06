dec34406f57f26b9dd98bf21c241c400e9340e97
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-06 10:14:47 -0500

    add class 78

---
+++ 09 - Properties and Indexes/ClassLibrary1/Class1.cs
@@ -0,0 +1,56 @@
+﻿public class Employee
+{
+
+    // instance fields
+    private int _id;
+    private string _name;
+    private string _job;
+    // static field
+    private static string _companyName;
+
+    // instance properties
+    public int Id
+    {
+        set { _id = value; }
+        get { return _id; }
+    }
+
+    public string Name
+    {
+        set
+        {
+            if (value.Length >= 5)
+            {
+                _name = value;
+            }
+        }
+        get { return _name; }
+    }
+
+    public string Job
+    {
+        set { _job = value; }
+        get { return _job; }
+    }
+
+    // static property
+    public static string CompanyName
+    {
+        set { _companyName = value; }
+        get { return _companyName;  }
+    }
+
+    // instance constructor
+    public Employee() { }
+
+
+    // with parameters
+    public Employee(int id, string name, string job) 
+    {
+        _id = id;
+        _name = name;
+        _job = job;
+    }
+
+
+}
---
+++ 09 - Properties and Indexes/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // will create class based on constructor only
+        Employee emp1 = new Employee(1, "John Doe", "plumber");
+        Employee emp2 = new Employee(2, "Joseph Richards", "product owner");
+
+        Employee emp3 = new Employee();
+        // will pass through properties valutation, therefore name will be null
+        emp3.Id = 3;
+        emp3.Name = "A";
+        emp3.Job = "Software Engineer";
+
+        System.Console.WriteLine("Welcome to " + Employee.CompanyName);
+
+        System.Console.WriteLine("ID: " + emp1.Id);
+        System.Console.WriteLine("Name: " + emp1.Name);
+        System.Console.WriteLine("Job: " + emp1.Job);
+        System.Console.WriteLine();
+
+        System.Console.WriteLine("ID: " + emp2.Id);
+        System.Console.WriteLine("Name: " + emp2.Name);
+        System.Console.WriteLine("Job: " + emp2.Job);
+
+        System.Console.WriteLine();
+        System.Console.WriteLine("ID: " + emp3.Id);
+        System.Console.WriteLine("Name: " + emp3.Name);
+        System.Console.WriteLine("Job: " + emp3.Job);
+
+        System.Console.ReadKey();
+    }
+}
