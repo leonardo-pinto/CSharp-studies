4a0a34e4bc67a0277e1a70a1fef076827a8439a0
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-10 11:49:55 -0500

    add class 110 - creating structures

---
+++ 14 - Structures/StructuresExample/ClassLibrary1/Category.cs
@@ -0,0 +1,4 @@
+﻿public struct Category
+{
+    public int CategoryId { get; set; }
+}
---
+++ 14 - Structures/StructuresExample/StructuresExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        // create structure instance
+        // new keyword is not creating an object
+        // only initializes structure fields
+        // using parameless constructor
+        Category category = new Category()
+        {
+            CategoryId = 1,
+        };
+
+        WriteLine("Category ID: " + category.CategoryId);
+        ReadKey();
+    }
+}
