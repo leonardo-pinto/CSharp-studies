5cdf2d3bc13c6bcd109146e3466ec1f9ac915a95
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-12 10:59:53 -0500

    add class 125 - null coalescing operator

--- 17 - Nullable Type/NullableExamples/NullableExamples/Program.cs
+++ 17 - Nullable Type/NullableExamples/NullableExamples/Program.cs
@@ -10,17 +10,29 @@ class Program
         Person person3 = null;
 
         WriteLine("Number of childrens: " + person1.numberOfChildren);
+        WriteLine();
 
         // use HasValue to check if property is null
-        if (person2.numberOfChildren.HasValue)
-        {
-            // numberOfChildren is nullable
-            // not possible to assign it to int value type
-            // must use Value
-            int value = person2.numberOfChildren.Value;
-            WriteLine("Number of childrens: " + value);
-        }
+        // verbose syntax
+        //if (person2.numberOfChildren.HasValue)
+        //{
+        //    // numberOfChildren is nullable
+        //    // not possible to assign it to int value type
+        //    // must use Value
+        //    int value = person2.numberOfChildren.Value;
+        //    WriteLine("Number of childrens: " + value);
+        //}
 
+        // ternary operator
+        WriteLine("Example using ternary operator");
+        WriteLine(person1.numberOfChildren.HasValue ? person1.numberOfChildren.Value : 0);
+        WriteLine(person2.numberOfChildren.HasValue ? person2.numberOfChildren.Value : 0);
+
+        // coalescing operator
+        WriteLine("Example using coalescing operator");
+        // alternative argument type must be the same field type (int)
+        WriteLine(person1.numberOfChildren ?? 0);
+        WriteLine(person2.numberOfChildren ?? 0);
         ReadKey();
     }
 }
