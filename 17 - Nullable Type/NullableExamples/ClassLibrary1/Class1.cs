56e3e21afcca2b35aef01ce3d9d100ca8dae5c07
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-12 10:39:52 -0500

    add class 124 - nullable types

---
+++ 17 - Nullable Type/NullableExamples/ClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿public class Person
+{
+    // nullable fields
+    // value types are not nullable, need to set manually as nullable
+    public int? numberOfChildren;
+    // alternative syntax
+    // public Nullable<int> numberOfChildren;
+}
---
+++ 17 - Nullable Type/NullableExamples/NullableExamples/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+class Program
+{
+    static void Main()
+    {
+        Person person1 = new Person() { numberOfChildren = 1 };
+        Person person2 = new Person() { numberOfChildren = null };
+        // reference types as classes are nullable by default
+        Person person3 = null;
+
+        WriteLine("Number of childrens: " + person1.numberOfChildren);
+
+        // use HasValue to check if property is null
+        if (person2.numberOfChildren.HasValue)
+        {
+            // numberOfChildren is nullable
+            // not possible to assign it to int value type
+            // must use Value
+            int value = person2.numberOfChildren.Value;
+            WriteLine("Number of childrens: " + value);
+        }
+
+        ReadKey();
+    }
+}
