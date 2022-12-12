5edee27300f84700c8f6dbe84a20dca602f81eb4
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-12 11:59:06 -0500

    add class 128 - pattern matching

---
+++ 18 - Methods Extension and Pattern Matching/PatternMatchingExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassLibrary1
+{
+    // parent class
+    public class ParentClass
+    {
+        public int X { get; set; }
+    }
+
+    // child class
+    public class ChildClass : ParentClass
+    {
+        public int Y { get; set; }  
+    }
+}
---
+++ 18 - Methods Extension and Pattern Matching/PatternMatchingExample/PatternMatchingExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using ClassLibrary1;
+using static System.Console;
+
+namespace PatternMatchingExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // reference variable of parent class
+            ParentClass sample;
+
+            // object of child class
+            sample = new ChildClass() { X = 1, Y = 2 };
+
+            // parent class type reference value !
+            WriteLine(sample.X);
+            // not possible to access Y field
+            // WriteLine(sample.Y);
+
+            // access parent class members
+            //if (sample is ChildClass)
+            //{
+            //    WriteLine("If statement");
+            //    ChildClass temp = (ChildClass)sample;
+            //    WriteLine(temp.Y);
+            //}
+            if (sample is ChildClass temp)
+            {
+                WriteLine("If statement");
+                WriteLine(temp.Y);
+            }
+
+            ReadKey();
+        }
+    }
+}
