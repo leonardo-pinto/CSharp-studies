cc7916594a469b53504fb20fb27d7ef45aaf4a56
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-12 09:19:13 -0500

    add class 121 - multiple generic parameters

--- 16 - Generics/GenericsExample/ClassLibrary1/Class1.cs
+++ 16 - Generics/GenericsExample/ClassLibrary1/Class1.cs
@@ -1,8 +1,11 @@
 ﻿// Generic class
-
-public class User<T>
+// any letter can be used to determine generic classes
+public class User<T1, T2>
 {
     // generic field
-    public T registrationStatus;
+    public T1 registrationStatus;
+    // multiple generic fields
+    public T2 age;
 }
+
 
--- 16 - Generics/GenericsExample/GenericsExample/Program.cs
+++ 16 - Generics/GenericsExample/GenericsExample/Program.cs
@@ -5,14 +5,16 @@ class Program
     static void Main()
     {
         // create object of generic class
-        User<int> user1 = new User<int>();
+        User<int, int> user1 = new User<int, int>();
 
         // using distinct field type to generic class
-        User<bool> user2 = new User<bool>();
+        User<bool, int> user2 = new User<bool, int>();
 
         // set value into generic field
         user1.registrationStatus = 1;
+        user1.age = 30;
         user2.registrationStatus = false;
+        user2.age = 25;
 
         WriteLine("User 1 registration status: " + user1.registrationStatus);
         WriteLine("User 2 registration status: " + user2.registrationStatus);
