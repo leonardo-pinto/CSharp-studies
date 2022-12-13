29dee56c2d71b537d80f7eebc4adb8486b258ab6
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-13 18:24:25 -0500

    add class 134 - IDisposable

---
+++ 19 - GC, Destructors and IDisposable/IDisposable/ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace Sample
+{
+    public class DisposableExample : System.IDisposable
+    {
+        // constructor
+        public DisposableExample()
+        {
+            System.Console.WriteLine("Constructor executed");
+        }
+
+        // method
+        public void MethodExample()
+        {
+            System.Console.WriteLine("MethodExample executed");
+        }
+
+        // dispose
+        public void Dispose()
+        {
+            System.Console.WriteLine("Dispose executed");
+        }
+    }
+}
---
+++ 19 - GC, Destructors and IDisposable/IDisposable/IDisposable/Program.cs
@@ -0,0 +1,15 @@
+﻿using Sample;
+class Program
+{
+    static void Main()
+    {
+        // create object using "using structure"
+        using (DisposableExample sample = new DisposableExample())
+        {
+            sample.MethodExample();
+        } // dispose is executed after end of using block
+
+        System.Console.WriteLine("Execution after using block");
+        System.Console.ReadKey();
+    }
+}
