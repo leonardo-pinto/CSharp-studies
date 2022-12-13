1c6cc60b5ab613d646b71d28e6e53cd7ec78ce97
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-13 18:05:20 -0500

    add class 133 - destructors

---
+++ 19 - GC, Destructors and IDisposable/DestructorsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿public class Sample
+{
+    // constructor
+    public Sample() 
+    {
+        System.Console.WriteLine("Constructor was executed");
+    }
+
+    // destructor
+    ~Sample()
+    {
+        System.Console.WriteLine("Destructor was executed");
+    }
+}
---
+++ 19 - GC, Destructors and IDisposable/DestructorsExample/DestructorsExample/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // create object of class
+        Sample sample = new Sample();
+
+    }
+    // destructor is executed automatically
+}
