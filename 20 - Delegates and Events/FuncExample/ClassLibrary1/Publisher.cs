5478358f59006cdc1b1adfa0d664c6dc888221a7
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 12:10:35 -0500

    add class 143 - Func

---
+++ 20 - Delegates and Events/FuncExample/ClassLibrary1/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Publisher
+    {
+        public event Func<int, int, int> MyEvent;
+
+        public int RaiseEvent(int a, int b)
+        {
+            if (MyEvent != null)
+            {
+                return MyEvent.Invoke(a, b);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ 20 - Delegates and Events/FuncExample/FuncExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Publisher publisher = new Publisher();
+
+            publisher.MyEvent += (a, b) => a + b;
+
+            System.Console.WriteLine(publisher.RaiseEvent(1, 1));
+            System.Console.ReadKey();
+        }
+    }
+}
