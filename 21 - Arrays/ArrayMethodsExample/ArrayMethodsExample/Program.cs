75e24c5806a0d250a5ead247f6ce40e270dfb44e
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 18:23:38 -0500

    add class 155 - BinarySearch

--- 21 - Arrays/ArrayMethodsExample/ArrayMethodsExample/Program.cs
+++ 21 - Arrays/ArrayMethodsExample/ArrayMethodsExample/Program.cs
@@ -10,8 +10,14 @@ namespace ArrayMethodsExample
             int[] numbersArray = new int[3] {1, 2, 3};
 
             // search for 2 in the array
+            // example of IndexOf
             Console.WriteLine("IndexOf 2: " + Array.IndexOf(numbersArray, 2));
             Console.WriteLine("Unexistent indexOf: " + Array.IndexOf(numbersArray, 50));
+            
+            // example of BinarySearch
+            // can only be used in sorted arrays
+            Console.WriteLine(Array.BinarySearch(numbersArray, 3));
+            
             Console.ReadKey();
         }
     }
