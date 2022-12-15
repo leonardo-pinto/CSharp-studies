c4cf7361d25c2da92e548c4158ab32d92f0ee198
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 17:37:59 -0500

    add class 150 - creating arrays

---
+++ 21 - Arrays/ArraysExample/ArraysExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArraysExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an array
+            int[] a = new int[3] {10,20,30};
+
+            // display element values
+            Console.WriteLine("a[0] " + a[0]);
+            Console.WriteLine("a[1] " + a[1]);
+            Console.ReadKey();
+        }
+    }
+}
