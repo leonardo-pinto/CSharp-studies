98e82d7b435fc9ff3492c71ee42f5350e21050bc
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 12:54:24 -0500

    add class 148 - expressions bodied members

---
+++ 20 - Delegates and Events/ExpressionBodiedMethods/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary1
+{
+    public class Student
+    {
+        // private fields
+        private string _studentName;
+
+        public Student(string studentName)
+        {
+            _studentName = studentName;
+        }
+
+        // example of constructors
+        // public Student() => _studentName = "Example";
+
+        // public method
+        //public int GetStudentNameLength()
+        //{
+        //    return _studentName.Length;
+        //}
+        
+        // expressions bodied method syntax
+        public int GetStudentNameLength() => _studentName.Length;
+    }
+}
---
+++ 20 - Delegates and Events/ExpressionBodiedMethods/ExpressionBodiedMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace ExpressionBodiedMethods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Student student = new Student("Leonardo") ;
+
+            Console.WriteLine(student.GetStudentNameLength());
+            Console.ReadKey();
+        }
+    }
+}
