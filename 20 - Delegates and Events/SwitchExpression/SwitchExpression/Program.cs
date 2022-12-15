87abce17ac2e5e461af522963224394d12759917
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 13:08:24 -0500

    add class 149 - switch expression

---
+++ 20 - Delegates and Events/SwitchExpression/SwitchExpression/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SwitchExpression
+{
+    class Program
+    {
+        static void Main()
+        {
+            int operation = 2; // several cases
+            string result;
+
+            // switch operation
+            result = operation switch
+            {
+                1 => "Customer",
+                2 => "Employee",
+                _ => "Not available"
+            };
+
+            System.Console.WriteLine(result);
+            System.Console.ReadKey();
+
+        }
+    }
+}
