abc889d6ff8c63a6b5d2eb105a9a4d8f0e12344c
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 18:08:35 -0500

    add class 154 - indexOf

---
+++ 21 - Arrays/ArrayMethodsExample/ArrayMethodsExample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayMethodsExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create array
+            int[] numbersArray = new int[3] {1, 2, 3};
+
+            // search for 2 in the array
+            Console.WriteLine("IndexOf 2: " + Array.IndexOf(numbersArray, 2));
+            Console.WriteLine("Unexistent indexOf: " + Array.IndexOf(numbersArray, 50));
+            Console.ReadKey();
+        }
+    }
+}
