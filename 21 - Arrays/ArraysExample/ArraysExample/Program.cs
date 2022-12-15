2d62810cd04414a721808c3d3582cc42a6b9f2b3
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 17:52:33 -0500

    add class 152 - arrays with foreach loops

--- 21 - Arrays/ArraysExample/ArraysExample/Program.cs
+++ 21 - Arrays/ArraysExample/ArraysExample/Program.cs
@@ -12,11 +12,20 @@ namespace ArraysExample
             // display element values
             //Console.WriteLine("a[0] " + a[0]);
             //Console.WriteLine("a[1] " + a[1]);
-            
+
             // display elements using for loop
+            Console.WriteLine("For loop");
             for (int index = 0; index < a.Length; index++)
             {
                 Console.WriteLine(a[index]);
+            }
+
+            Console.WriteLine();
+            // display elements using foreach loop
+            Console.WriteLine("Foreach loop");
+            foreach (int element in a)
+            {
+                Console.WriteLine(element);
             }
 
             Console.ReadKey();
