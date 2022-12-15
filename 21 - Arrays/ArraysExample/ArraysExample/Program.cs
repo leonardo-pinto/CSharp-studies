600e822d951b36a89ac51bfa3997708848080f7a
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-15 17:46:56 -0500

    add class 151 - arrays with for loop

--- 21 - Arrays/ArraysExample/ArraysExample/Program.cs
+++ 21 - Arrays/ArraysExample/ArraysExample/Program.cs
@@ -10,8 +10,15 @@ namespace ArraysExample
             int[] a = new int[3] {10,20,30};
 
             // display element values
-            Console.WriteLine("a[0] " + a[0]);
-            Console.WriteLine("a[1] " + a[1]);
+            //Console.WriteLine("a[0] " + a[0]);
+            //Console.WriteLine("a[1] " + a[1]);
+            
+            // display elements using for loop
+            for (int index = 0; index < a.Length; index++)
+            {
+                Console.WriteLine(a[index]);
+            }
+
             Console.ReadKey();
         }
     }
