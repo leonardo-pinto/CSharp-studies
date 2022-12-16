2667f6b6055d29bea3c57f9f6babb7f4bd71338f
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-16 10:33:35 -0500

    add class 167 - list

---
+++ 22 - Collections/ListExample/ListExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create reference variable for List class
+            // and create object of List class
+
+            // number 10 represents the initial list capacity
+            // therefore, until list has 10 elements, list is
+            // not reconstructed
+            List<int> myList = new List<int>(10) { 1, 2, 3 };
+
+            foreach(int element in myList)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
