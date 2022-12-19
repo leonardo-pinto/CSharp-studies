13bc2ea22dcc90efab9e2d909482dba6d9e32034
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-19 17:39:13 -0500

    add class 181 - Stack

---
+++ 22 - Collections/StackExample/StackExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace StackExample
+{
+    class Student
+    {
+        public int Marks { get; set; }
+        public int Rank { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            // create object of Stack
+            Stack <Student> marks =  new Stack<Student>();
+
+            // Add
+            //marks.Push(45);
+            //marks.Push(20);
+            //marks.Push(90);
+            marks.Push(new Student() { Marks = 45 });
+            marks.Push(new Student() { Marks = 61 });
+            marks.Push(new Student() { Marks = 20 });
+
+            marks.Pop();
+
+            // foreach
+            foreach(Student student in marks)
+            {
+                Console.WriteLine(student.Marks);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
