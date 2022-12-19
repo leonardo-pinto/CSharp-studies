2d0657451e9658556a65c8e1d73c3daf8fc3c305
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-19 16:57:09 -0500

    add class 180 - ArrayList

---
+++ 22 - Collections/ArrayListExample/ArrayListExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+            // create object of ArrayList class
+            ArrayList arrayList = new ArrayList()
+            { 1, "any text" };
+
+            arrayList.Add(1.1);
+
+            Employee emp = new Employee() { Name = "Joseph Richards" };
+
+            arrayList.Add(emp);
+
+            foreach(var item in arrayList)
+            {
+                if (item is Employee employee)
+                {
+                    Console.WriteLine(employee.Name);
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                };
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
