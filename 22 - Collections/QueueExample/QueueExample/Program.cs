ed9bc6cbef945b75c55870c1d86d207e48b79495
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-20 08:35:25 -0500

    add class 182 - Queue

---
+++ 22 - Collections/QueueExample/QueueExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create an object of Queue class
+            Queue<string> queue = new Queue<string>();
+
+            // enqueue (add)
+            queue.Enqueue("Task 1");
+            queue.Enqueue("Task 2");
+            queue.Enqueue("Task 3");
+
+            // foreach
+            foreach(string item in queue)
+            {
+                Console.WriteLine(item);
+            };
+
+            // dequeue (remove)
+            string dequeuedItem = queue.Dequeue();
+            Console.WriteLine("Dequeued item " + dequeuedItem);
+
+            // Peek
+            // return next element
+            string peekExample = queue.Peek();
+            Console.WriteLine("Peek: " + peekExample);
+            Console.ReadKey();
+
+        }
+    }
+}
