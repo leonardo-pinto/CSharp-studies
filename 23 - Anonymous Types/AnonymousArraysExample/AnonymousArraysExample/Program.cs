887afc97d622201b9c7cca0897b13eb0ea462d21
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-21 15:05:39 -0500

    add class 200 - Anonymous Arrays

---
+++ 23 - Anonymous Types/AnonymousArraysExample/AnonymousArraysExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousArraysExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create anonymous array / implicitly typed array
+            var persons = new[]
+            {
+                new { PersonName = "Joseph", PersonAge = 40 },
+                new { PersonName = "Richards", PersonAge = 55},
+                // elements with other properties or types
+                // are not allowed
+                //new { PersonName = "Zé Urso" }
+            };
+        }
+    }
+}
