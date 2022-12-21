1c944096a1df85867213f626a99874db1332736a
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-21 14:52:21 -0500

    add class 198 - Anonymous Types

---
+++ 23 - Anonymous Types/AnonymousObjectsExample/AnonymousObjectsExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace AnonymousObjectsExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // create object of Person class
+            Person p = new Person();
+
+            // call methods
+            //p.GetPersonName();
+            //p.GetPersonAge();
+
+            // combine both properties in the same method
+            // using anonymous types
+            var person = new { PersonName = p.GetPersonName(), PersonAge = p.GetPersonAge() };
+
+            // print
+            Console.WriteLine(person.PersonName);
+            Console.WriteLine(person.PersonAge);
+
+            // readonly
+            person.PersonAge = 10;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 23 - Anonymous Types/AnonymousObjectsExample/ClassLibrary1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Person
+    {
+        public string GetPersonName()
+        {
+            return "Smith";
+        }
+
+        public int GetPersonAge()
+        {
+            return 29;
+        }
+    }
+}
