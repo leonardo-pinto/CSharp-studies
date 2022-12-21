c4f462d1b3207f957bc616627934cfd1be55d90a
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-21 15:41:40 -0500

    add class 202 - Value Tuples

---
+++ 23 - Anonymous Types/ValueTuplesExample/ClassLibrary1/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Customer
+    {
+        // example of tuple value as return type
+        public (int customerId, string customerName) GetCustomerDetails()
+        {
+            return (101, "Scott");
+        }
+    }
+}
---
+++ 23 - Anonymous Types/ValueTuplesExample/ValueTuplesExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+
+namespace ValueTuplesExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Customer customer= new Customer();
+
+            (int customerId, string customerName) cust = customer.GetCustomerDetails();
+
+            Console.WriteLine(cust.customerName);
+            Console.WriteLine(cust.customerId);
+
+            Console.ReadKey();
+        }
+    }
+}
