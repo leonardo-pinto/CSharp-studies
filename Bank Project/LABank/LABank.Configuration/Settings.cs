f3e433de1697dda4d3d5d38ec3596854a3e581bf
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-22 09:00:09 -0500

    add class 2011 - Adding Configuration Settings

---
+++ Bank Project/LABank/LABank.Configuration/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LABank.Configuration
+{
+    /// <summary>
+    /// Project level configuration settings
+    /// </summary>
+    public static class Settings
+    {
+        /// <summary>
+        /// Customer number start from 1001;incremented by 1
+        /// </summary>
+        public static long BaseCustomerNo { get; set; } = 1000;
+    }
+}
