1d73c6d6ed196ad9d0586e9bf955f1d04065f9da
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-23 11:02:14 -0500

    add class 225 - Converting Strings Part 2

--- 27 - Strings, DateTime and Math/StringsExample/StringsExample/Program.cs
+++ 27 - Strings, DateTime and Math/StringsExample/StringsExample/Program.cs
@@ -16,6 +16,25 @@ namespace StringsExample
             Console.WriteLine("ToUpper: " + str.ToUpper()); // DEVELOPER
             Console.WriteLine("ToLower: " + str.ToLower()); // developer
             Console.WriteLine("Substring: " + str.Substring(1, 3)); // eve
+            Console.WriteLine("Replace: " + str.Replace("e", "a")); // Davalopar
+            string[] strSplit = str.Split('e');
+            Console.WriteLine("Split: ");
+            foreach(string item in strSplit)
+            {
+                Console.WriteLine(item);
+            }
+
+            string trimExample = "   hello   ";
+            string trimResult = trimExample.Trim();
+            Console.WriteLine("Trim: " + trimResult);
+
+            string toCharArrayExample = "any string";
+            char[] toCharArrayResult = toCharArrayExample.ToCharArray();
+            Console.WriteLine("ToCharArray:");
+            foreach (char item in toCharArrayResult)
+            {
+                Console.WriteLine(item);
+            }
 
             Console.ReadKey();
         }
