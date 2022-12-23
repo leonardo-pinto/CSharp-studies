b248190f9d9e4754937239b0074c238b92573b33
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-23 09:05:02 -0500

    add class 215 - OrderBy

--- 26 - LINQ/LINQExample/LINQExample/Program.cs
+++ 26 - LINQ/LINQExample/LINQExample/Program.cs
@@ -10,6 +10,7 @@ namespace LINQExample
         public string Name { get; set; }
         public string City { get; set; }
         public string Job { get; set; }
+        public int Salary { get; set; }
     }
 
     internal class Program
@@ -19,19 +20,40 @@ namespace LINQExample
             // collection of objects
             List<Employee> employees = new List<Employee>()
             { 
-                new Employee(){ Id = 1, Name = "Joseph", City = "Botucatu", Job = "Manager"},
-                new Employee(){ Id = 2, Name = "John Doe", City = "Dallas", Job = "Developer"},
-                new Employee(){ Id = 3, Name = "Zé Urso", City = "Botucatu", Job = "Developer"},
-                new Employee(){ Id = 4, Name = "Leonardo", City = "Jundiai", Job = "Analyst"},
-                new Employee(){ Id = 5, Name = "Josephina", City = "Avaré", Job = "QA"},
+                new Employee(){ Id = 1, Name = "Joseph", City = "Botucatu", Job = "Manager", Salary = 1000 },
+                new Employee(){ Id = 2, Name = "John Doe", City = "Dallas", Job = "Developer", Salary = 5000 },
+                new Employee(){ Id = 3, Name = "Zé Urso", City = "Botucatu", Job = "Developer", Salary = 3000 },
+                new Employee(){ Id = 4, Name = "Leonardo", City = "Jundiai", Job = "Analyst", Salary = 750 },
+                new Employee(){ Id = 5, Name = "Josephina", City = "Avaré", Job = "QA", Salary = 1500 },
             };
 
             IEnumerable<Employee> result = employees.Where(employee => employee.City == "Botucatu");
 
-            foreach(Employee employee in result)
+            // where example
+            Console.WriteLine("\nWhere example: ");
+            foreach (Employee employee in result)
             {
                 Console.WriteLine(employee.Id + ", " + employee.Name + ", " + employee.City + ", " + employee.Job);
             }
+
+            // OrderyBy example
+            // descending: OrderByDescending()
+            IOrderedEnumerable<Employee> orderBySalary = employees.OrderBy(emp => emp.Salary);
+            Console.WriteLine("\nOrderBy example: ");
+            foreach (Employee employee in orderBySalary)
+            {
+                Console.WriteLine(employee.Id + ", " + employee.Name + ", " + employee.City + ", " + employee.Job + ", " + employee.Salary);
+            }
+
+            // using second ordering parameter
+            // ThenBy
+            IOrderedEnumerable<Employee> orderByJob = employees.OrderBy(emp => emp.Job).ThenBy(emp => emp.Salary);
+            Console.WriteLine("\nOrderBy and ThenBy example: ");
+            foreach (Employee employee in orderByJob)
+            {
+                Console.WriteLine(employee.Id + ", " + employee.Name + ", " + employee.City + ", " + employee.Job + ", " + employee.Salary);
+            }
+
             Console.ReadKey();
         }
     }
