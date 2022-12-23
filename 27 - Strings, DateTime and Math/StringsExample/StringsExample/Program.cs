7dae0a83c51743dda6738d0a194d049e52ac5ae7
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-23 10:53:20 -0500

    add class 224 - Converting Strings Part 1

---
+++ 27 - Strings, DateTime and Math/StringsExample/StringsExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str = "Developer";
+
+            Console.WriteLine("Initial string: " + str);
+            Console.WriteLine("ToUpper: " + str.ToUpper()); // DEVELOPER
+            Console.WriteLine("ToLower: " + str.ToLower()); // developer
+            Console.WriteLine("Substring: " + str.Substring(1, 3)); // eve
+
+            Console.ReadKey();
+        }
+    }
+}
