91673b368d1f0508f9ba83e14863dc1f99507a28
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-24 11:12:17 -0500

    add class 235 - StringBuilder

---
+++ 27 - Strings, DateTime and Math/StringBuilderExample/StringBuilderExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] words = new string[] { "Hello", "how", "are", "you", "?" };
+            // StringBuilder
+            StringBuilder builder = new StringBuilder();
+
+            foreach(string word in words)
+            {
+                builder.Append(word);
+                builder.Append(" ");
+            }
+
+            Console.WriteLine(builder.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
