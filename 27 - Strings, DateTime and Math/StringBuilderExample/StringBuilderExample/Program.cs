16192b67842361e55614aa1afcb11a16f9baa261
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-24 15:11:02 -0500

    add class 236 - Methods of StringBuilder

--- 27 - Strings, DateTime and Math/StringBuilderExample/StringBuilderExample/Program.cs
+++ 27 - Strings, DateTime and Math/StringBuilderExample/StringBuilderExample/Program.cs
@@ -7,14 +7,16 @@ namespace StringBuilderExample
     {
         static void Main()
         {
-            string[] words = new string[] { "Hello", "how", "are", "you", "?" };
+            string[] words = new string[] { "Hello", "how", "are", "you", "?", "I", "am", "fine" };
             // StringBuilder
-            StringBuilder builder = new StringBuilder();
+            StringBuilder builder = new StringBuilder(20);
 
             foreach(string word in words)
             {
                 builder.Append(word);
                 builder.Append(" ");
+
+                Console.WriteLine(builder.ToString() + ", " + builder.Length + ", " + builder.Capacity);
             }
 
             Console.WriteLine(builder.ToString());
