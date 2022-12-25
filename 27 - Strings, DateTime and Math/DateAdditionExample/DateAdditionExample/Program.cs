2e538e5fc4eba40b951dc697725ee83ae6b94796
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-25 16:10:00 -0500

    add class 242 - Date Addition

---
+++ 27 - Strings, DateTime and Math/DateAdditionExample/DateAdditionExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAdditionExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DateTime dt = DateTime.Parse("2042-10-10");
+            DateTime addedDate = dt.AddDays(5);
+
+            // AddMinutes
+            // AddSeconds, Milliseconds, etc
+            // Add negative values to subtract;
+            Console.WriteLine(addedDate.ToString());   
+            Console.ReadKey();
+        }
+    }
+}
