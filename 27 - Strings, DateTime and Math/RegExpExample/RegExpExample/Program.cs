aa60d4afd098d64d5e4727b39b2624e9ebf86d8c
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-25 16:33:04 -0500

    add class 245 - Regular Expressions

---
+++ 27 - Strings, DateTime and Math/RegExpExample/RegExpExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RegExpExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // ^ represents value beginning
+            // $ represents value ending
+            // only numeric values
+            Regex regex = new Regex("^[0-9]*$");
+            Console.WriteLine("Enter a digit: ");
+            string inputValue = Console.ReadLine();
+            bool result = regex.IsMatch(inputValue);
+            Console.WriteLine(result);
+
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Regex alphabeticRegex = new Regex("^[A-Za-z]*$");
+            bool nameResult = alphabeticRegex.IsMatch(name);
+
+            Console.WriteLine("Name is valid? " + nameResult);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 27 - Strings, DateTime and Math/RegExpExample/RegExpExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("RegExpExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegExpExample")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("00ebf65c-3cf5-46db-b431-ee7c5a8ffdcf")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
