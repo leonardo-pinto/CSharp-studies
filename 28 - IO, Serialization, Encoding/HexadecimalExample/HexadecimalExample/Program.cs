6e0008593fb8d1f0e16ad8d91ab621f2b53301aa
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 10:06:23 -0500

    add class 249 - Hexadecimal Number System

---
+++ 28 - IO, Serialization, Encoding/HexadecimalExample/HexadecimalExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int initNumber = 15649;
+
+            string hexadecimalNumber = Convert.ToString(initNumber, 16);
+
+            int decimalNumber = Convert.ToInt32(hexadecimalNumber, 16);
+
+            Console.WriteLine(hexadecimalNumber);
+            Console.WriteLine(decimalNumber);
+            Console.ReadKey();
+        }
+    }
+}
