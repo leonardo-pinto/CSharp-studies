92cf0386ac210796788c9cb99c7d325977557fed
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 10:03:01 -0500

    add class 248 - Octal Number System

---
+++ 28 - IO, Serialization, Encoding/OctalExample/OctalExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctalExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int decimalNumber = 289;
+
+            // convert decimal to octal number system
+            string octalNumber = Convert.ToString(decimalNumber, 8);
+
+            // convert octal number to decimal number
+            int convertedNumber = Convert.ToInt32(octalNumber, 8);
+
+            Console.WriteLine(octalNumber);
+
+            Console.WriteLine(convertedNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
