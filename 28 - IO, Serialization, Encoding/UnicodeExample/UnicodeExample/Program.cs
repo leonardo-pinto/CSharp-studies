599e9cd398bbabfd7cbf7369ac764f11b7002166
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 10:46:58 -0500

    add class 252 - Unicode character encoding

---
+++ 28 - IO, Serialization, Encoding/UnicodeExample/UnicodeExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // \u indicates it is a unicode format
+            string code = "\u0543";
+            Console.WriteLine(code);
+
+
+            string sentence = "Hello, how are you";
+
+            byte[] sentenceInBytes = Encoding.Unicode.GetBytes(sentence);
+
+            string sentenceInString = Encoding.Unicode.GetString(sentenceInBytes);
+
+            Console.WriteLine(sentenceInString);
+
+            Console.ReadKey();
+        }
+    }
+}
