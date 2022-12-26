5199b310749eea92e5b5bfa926fa0180b2c69ed1
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 09:58:07 -0500

    add class 247 - Binary Number System

---
+++ 28 - IO, Serialization, Encoding/BinaryExample/BinaryExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // numbers are represented in decimal system
+            int decimalNumber = 13;
+
+            // binary number is represented as string
+            string binaryNumber = Convert.ToString(decimalNumber, 2);
+
+            Console.WriteLine(binaryNumber);
+
+            // convert to decimal number
+            Console.WriteLine(Convert.ToInt32(binaryNumber, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
