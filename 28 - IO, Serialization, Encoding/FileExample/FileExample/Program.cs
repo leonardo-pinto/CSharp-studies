795c144b148636a45d0260844608ef270c5636d7
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 11:38:21 -0500

    add class 255 - File class: Read and Write

--- 28 - IO, Serialization, Encoding/FileExample/FileExample/Program.cs
+++ 28 - IO, Serialization, Encoding/FileExample/FileExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace FileExample
 {
@@ -8,44 +9,72 @@ namespace FileExample
         static void Main()
         {
 
-            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice";
-            string fileName = "file_test.txt";
-            string anotherFileName = "file_copy_test.txt";
-            string moveFileName = "file_move_test.txt";
+            //string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice";
+            //string fileName = "file_test.txt";
+            //string anotherFileName = "file_copy_test.txt";
+            //string moveFileName = "file_move_test.txt";
 
-            string fullFilePath = $"{filePath}\\{fileName}";
-            // Create method
-            // create and opens the file
-            // need to execute Close() method
-            File.Create(fullFilePath).Close();
-            Console.WriteLine($"{fileName} was created successfully");
+            //string fullFilePath = $"{filePath}\\{fileName}";
+            //// Create method
+            //// create and opens the file
+            //// need to execute Close() method
+            //File.Create(fullFilePath).Close();
+            //Console.WriteLine($"{fileName} was created successfully");
 
 
-            // Exists method
-            Console.WriteLine("File exists? " + File.Exists(fullFilePath)); // true
-            Console.WriteLine("File exists? " + File.Exists("abuble")); // false
+            //// Exists method
+            //Console.WriteLine("File exists? " + File.Exists(fullFilePath)); // true
+            //Console.WriteLine("File exists? " + File.Exists("abuble")); // false
 
-            bool exists = File.Exists(fullFilePath);
-            if (exists)
+            //bool exists = File.Exists(fullFilePath);
+            //if (exists)
+            //{
+            //    // Copy method
+            //    // throws exception if anotherFileName already exists
+            //    File.Copy(fullFilePath, $"{filePath}\\{anotherFileName}");
+            //    Console.WriteLine($"Copied {fileName} to {anotherFileName}");
+
+
+            //    // Move method
+            //    File.Move($"{filePath}\\{anotherFileName}", $"{filePath}\\{moveFileName}");
+            //    Console.WriteLine("File moved successfully");
+
+            //    // Delete method
+            //    File.Delete($"{filePath}\\{moveFileName}");
+            //    Console.WriteLine("File deleted successfully");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File not found");
+            //}
+
+
+            //string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\dog.txt";
+            //string content = "The dog is one of the common domestic animals";
+            //// WriteAllText method
+            //// overwrite file if it already exists
+            //File.WriteAllText(filePath, content);
+            //Console.WriteLine("File created successfully");
+
+            //// ReadAllText
+            //string fileContent = File.ReadAllText(filePath);
+
+            //Console.WriteLine("File content: ");
+            //Console.WriteLine(fileContent);
+
+            // collections
+            List<string> countries = new List<string>() { "Russia", "China", "Japan" };
+
+            // file path
+            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\countries.txt";
+            File.WriteAllLines(filePath, countries);
+
+            string[] lineContent = File.ReadAllLines(filePath);
+            foreach(string line in lineContent)
             {
-                // Copy method
-                // throws exception if anotherFileName already exists
-                File.Copy(fullFilePath, $"{filePath}\\{anotherFileName}");
-                Console.WriteLine($"Copied {fileName} to {anotherFileName}");
-
-
-                // Move method
-                File.Move($"{filePath}\\{anotherFileName}", $"{filePath}\\{moveFileName}");
-                Console.WriteLine("File moved successfully");
-
-                // Delete method
-                File.Delete($"{filePath}\\{moveFileName}");
-                Console.WriteLine("File deleted successfully");
+                Console.WriteLine(line);
             }
-            else
-            {
-                Console.WriteLine("File not found");
-            }
+
 
             Console.ReadKey();
         }
