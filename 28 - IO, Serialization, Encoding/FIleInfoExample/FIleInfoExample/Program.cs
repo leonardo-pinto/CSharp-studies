e3723c56f1ca75f67e44294ec0a8b3b76e2a7465
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 12:06:02 -0500

    add class 257 - FileInfo class properties

--- 28 - IO, Serialization, Encoding/FIleInfoExample/FIleInfoExample/Program.cs
+++ 28 - IO, Serialization, Encoding/FIleInfoExample/FIleInfoExample/Program.cs
@@ -8,31 +8,43 @@ namespace FIleInfoExample
     {
         static void Main()
         {
-            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan.txt";
-            string filePathToCopy = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan_copy.txt";
-            string filePathMove = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan_move.txt";
+            //string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan.txt";
+            //string filePathToCopy = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan_copy.txt";
+            //string filePathMove = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan_move.txt";
 
-            // file info is not static
+            //// file info is not static
+            //FileInfo fileInfo = new FileInfo(filePath);
+            //// create and close the file
+            //fileInfo.Create().Close();
+
+            //Console.WriteLine(fileInfo.Name + " created");
+
+            //// CopyTo method
+            //// returns a new instance of the FileInfo class
+            //// any instance refers to a specific file
+            //// throws exception if it already exists
+            //FileInfo copiedFileInfo = fileInfo.CopyTo(filePathToCopy);
+
+            //Console.WriteLine(copiedFileInfo.Name + " created");
+
+            //// MoveTo method
+            //copiedFileInfo.MoveTo(filePathMove);
+            //Console.WriteLine(copiedFileInfo.Name + " moved");
+
+            //copiedFileInfo.Delete();
+            //Console.WriteLine(copiedFileInfo.Name + " deleted");
+
+            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\japan.txt";
             FileInfo fileInfo = new FileInfo(filePath);
             // create and close the file
             fileInfo.Create().Close();
 
-            Console.WriteLine(fileInfo.Name + " created");
-
-            // CopyTo method
-            // returns a new instance of the FileInfo class
-            // any instance refers to a specific file
-            // throws exception if it already exists
-            FileInfo copiedFileInfo = fileInfo.CopyTo(filePathToCopy);
-
-            Console.WriteLine(copiedFileInfo.Name + " created");
-
-            // MoveTo method
-            copiedFileInfo.MoveTo(filePathMove);
-            Console.WriteLine(copiedFileInfo.Name + " moved");
-
-            copiedFileInfo.Delete();
-            Console.WriteLine(copiedFileInfo.Name + " deleted");
+            Console.WriteLine(fileInfo.CreationTime);
+            Console.WriteLine(fileInfo.LastWriteTime);
+            Console.WriteLine(fileInfo.FullName);
+            Console.WriteLine(fileInfo.Name);
+            Console.WriteLine(fileInfo.DirectoryName);
+            Console.WriteLine(fileInfo.Length);
 
             Console.ReadKey();
         
