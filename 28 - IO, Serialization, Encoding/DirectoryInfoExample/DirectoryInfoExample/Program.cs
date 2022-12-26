5d3a3565bb69ac40bf2654a45c11cbc1ba327a46
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-26 12:59:49 -0500

    add class 260 - DirectoryInfo class

---
+++ 28 - IO, Serialization, Encoding/DirectoryInfoExample/DirectoryInfoExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DirectoryInfoExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo("C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\countries");
+
+            // Create directory
+            directoryInfo.Create();
+            Console.WriteLine("Countries folder created");
+
+            directoryInfo.CreateSubdirectory("india");
+            directoryInfo.CreateSubdirectory("russia");
+
+            new FileInfo(directoryInfo.FullName + "\\capitals.txt").Create();
+
+            Console.ReadKey();
+        }
+    }
+}
