aa8960628bd452a506a17e916870550450f6ee8b
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 13:37:53 -0500

    add class 270 - Binary Serialization Part 1

---
+++ 28 - IO, Serialization, Encoding/BinarySerializationExample/BinarySerializationExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinarySerializationExample
+{
+    // add decorator to serialize class
+    [Serializable]
+    public class Country
+    {
+        public short CountryId { get; set; }
+        public string CountryName { get; set; }
+        public long Population { get; set; }
+        public string Region { get; set; }
+
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            Country country = new Country() { CountryId = 1, CountryName = "Russia", Population = 10000, Region = "Eastern Europe" };
+
+            // create FileStream
+            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\russia.txt";
+            FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+            // create BinaryFormatter
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fileStream, country);
+            fileStream.Close();
+
+            Console.WriteLine("File serialized");
+            Console.ReadKey();
+
+        
+        }
+    }
+}
