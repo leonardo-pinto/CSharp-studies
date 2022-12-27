adbd6ce289242920be1611d1266f7fb0f8c87606
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 10:34:31 -0500

    add class 264 - FileStream class Part 2

--- 28 - IO, Serialization, Encoding/FileStreamExample/FileStreamExample/Program.cs
+++ 28 - IO, Serialization, Encoding/FileStreamExample/FileStreamExample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Text;
 
 namespace FileStreamExample
 {
@@ -14,6 +15,24 @@ namespace FileStreamExample
             // both syntax are the same, since File.Create returns a FileStream
             //FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);       
             FileStream fileStream = File.Create(filePath);
+
+            // create content
+            string content = "Cat is one of the domestic animals";
+
+            // FileStream uses byte
+            byte[] byteContent = Encoding.ASCII.GetBytes(content);
+
+            fileStream.Write(byteContent, 0, byteContent.Length);
+
+            string content2 = "more content";
+            byte[] byteContent2 = Encoding.ASCII.GetBytes(content2);
+
+            fileStream.Write(byteContent2, 0, byteContent2.Length);
+
+            Console.WriteLine("cat.txt was created");
+            fileStream.Close();
+
+            Console.ReadKey();
         }
     }
 }
