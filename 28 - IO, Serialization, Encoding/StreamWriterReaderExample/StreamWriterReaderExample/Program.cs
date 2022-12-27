9b7f0f130241b118a11e2c40f28e334535f20f80
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 10:58:23 -0500

    add class 266 - StreamWriter and StreamReader part 1

---
+++ 28 - IO, Serialization, Encoding/StreamWriterReaderExample/StreamWriterReaderExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamWriterReaderExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\europe.txt";
+           
+            // using construct is recommended
+            // file is closed after closing brackets
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            {
+                streamWriter.WriteLine("Halo");
+
+                streamWriter.WriteLine("how are you?");
+
+            };
+        }
+    }
+}
