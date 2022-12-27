6be7448af0bf833ab75e9bd82f6e00d40963079c
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 13:19:15 -0500

    add class 269 - BinaryWriter and BinaryReader Part 2

--- 28 - IO, Serialization, Encoding/BinaryWriterReaderExample/BinaryWriterReaderExample/Program.cs
+++ 28 - IO, Serialization, Encoding/BinaryWriterReaderExample/BinaryWriterReaderExample/Program.cs
@@ -12,22 +12,38 @@ namespace BinaryWriterReaderExample
         static void Main()
         {
             short countryId = 1;
-            string countrName = "Belarus";
+            string countryName = "Belarus";
             long population = 5505400;
             string region = "Eastern Europe";
 
             string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\belarus.txt";
             FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
             // writes information in binary system
+            // BinaryWriter
             using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
             {
                 binaryWriter.Write(countryId);
+                binaryWriter.Write(countryName);
                 binaryWriter.Write(population);
                 binaryWriter.Write(region);
-                binaryWriter.Write(countrName);
             }
 
             Console.WriteLine("belarus.txt was created");
+            FileStream filestream2 = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            // BinaryReader
+            using (BinaryReader binaryReader = new BinaryReader(filestream2))
+            {
+                int countryIdFromFile = binaryReader.ReadInt16();
+                string countryNameFromFile = binaryReader.ReadString();
+                long populationFromFile = binaryReader.ReadInt64();
+                string countryRegionFromFile = binaryReader.ReadString();
+
+
+                Console.WriteLine("Country id: " + countryIdFromFile);
+                Console.WriteLine("Country name: " + countryNameFromFile);
+                Console.WriteLine("Country population: " + populationFromFile);
+                Console.WriteLine("Country region: " + countryRegionFromFile);
+            }
 
             Console.ReadKey();
         }
