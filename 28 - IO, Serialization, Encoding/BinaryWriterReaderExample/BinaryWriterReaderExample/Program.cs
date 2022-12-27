2ead3c8aae49665158b2de20bcbfafe5ade65a9a
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 13:06:02 -0500

    add class 268 - BinaryWriter and BinaryReader Part 1

---
+++ 28 - IO, Serialization, Encoding/BinaryWriterReaderExample/BinaryWriterReaderExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BinaryWriterReaderExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            short countryId = 1;
+            string countrName = "Belarus";
+            long population = 5505400;
+            string region = "Eastern Europe";
+
+            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\belarus.txt";
+            FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+            // writes information in binary system
+            using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+            {
+                binaryWriter.Write(countryId);
+                binaryWriter.Write(population);
+                binaryWriter.Write(region);
+                binaryWriter.Write(countrName);
+            }
+
+            Console.WriteLine("belarus.txt was created");
+
+            Console.ReadKey();
+        }
+    }
+}
