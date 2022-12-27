cb2f9197a3b02474a09dc8dfe3c9897f855da44b
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 10:00:35 -0500

    add class 262 - DriveInfo class

---
+++ 28 - IO, Serialization, Encoding/DriveInfoExample/DriveInfoExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace DriveInfoExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            DriveInfo driveInfo = new DriveInfo("c:");
+
+            Console.WriteLine("Name: " + driveInfo.Name);
+            Console.WriteLine("DriveType: " + driveInfo.DriveType);
+            Console.WriteLine("Volume label: " + driveInfo.VolumeLabel);
+            Console.WriteLine("Root Directory: " + driveInfo.RootDirectory);
+            // data in Gb
+            Console.WriteLine("Total size: " + (driveInfo.TotalSize / 1024 / 1024 /1024) + "gb");
+            Console.WriteLine("Free space: " + (driveInfo.AvailableFreeSpace / 1024 / 1024 / 1024) + "gb");
+
+
+            Console.ReadKey();
+        }
+    }
+}
