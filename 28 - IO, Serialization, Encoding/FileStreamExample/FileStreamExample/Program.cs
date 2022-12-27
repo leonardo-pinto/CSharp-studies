7e77600ade88425b130cbeab91d009de86c13976
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-27 10:22:33 -0500

    add class 263 - FileStream class Part 1

---
+++ 28 - IO, Serialization, Encoding/FileStreamExample/FileStreamExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace FileStreamExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string filePath = "C:\\Users\\Leonardo\\Documents\\Projects\\CSharp-studies\\28 - IO, Serialization, Encoding\\practice\\cat.txt";
+
+            // both syntax are the same, since File.Create returns a FileStream
+            //FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write);       
+            FileStream fileStream = File.Create(filePath);
+        }
+    }
+}
