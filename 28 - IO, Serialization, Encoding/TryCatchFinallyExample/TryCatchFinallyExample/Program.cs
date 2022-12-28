3e584b2dddb923fcbfe9095e3632d5434bddd420
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-28 10:33:55 -0500

    add class 274 - Try-Catch-Finally

---
+++ 28 - IO, Serialization, Encoding/TryCatchFinallyExample/TryCatchFinallyExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinallyExample
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter first number: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Enter second number: ");
+                int b = int.Parse(Console.ReadLine());
+                int c = a/b;
+                // if b = 0, throw new DividedByZeroException
+                Console.WriteLine("Result of division: " + c);
+            }
+            // catches this specific exception
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
