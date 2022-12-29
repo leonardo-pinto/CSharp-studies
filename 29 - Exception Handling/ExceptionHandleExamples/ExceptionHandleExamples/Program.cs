a0922a3b8a0ab6d910575ba13a89ab34202a7629
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-29 10:45:26 -0500

    add class 277 - NullReferenceException

--- 29 - Exception Handling/ExceptionHandleExamples/ExceptionHandleExamples/Program.cs
+++ 29 - Exception Handling/ExceptionHandleExamples/ExceptionHandleExamples/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExceptionHandleExamples
 {
@@ -17,33 +15,68 @@ namespace ExceptionHandleExamples
     {
         static void Main()
         {
+            //// FormatException example
+            //try
+            //{
+            //    BankAccount bankAccount = new BankAccount();
+
+            //    Console.Write("Enter account holder name: ");
+            //    bankAccount.HolderName = Console.ReadLine();
+            //    Console.Write("Enter account number: ");
+
+            //    // when parsing strings, space or special characters
+            //    bankAccount.Number = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter account current balance: ");
+            //    bankAccount.CurrentBalance = double.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("\nNew bank account details:");
+            //    Console.WriteLine("Holder name: " + bankAccount.HolderName);
+            //    Console.WriteLine("Account number: " + bankAccount.Number);
+            //    Console.WriteLine("Current balance: " + bankAccount.CurrentBalance);
+
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine(ex.StackTrace);
+            //}
+
+            //Console.ReadKey();
+
+            //IndexOutOfRangeException
+
             try
             {
-                //FormatException
+                BankAccount[] accounts = new BankAccount[]
+                {
+                   new BankAccount(){ HolderName = "Joseph", Number = 1, CurrentBalance = 100 },
+                   new BankAccount(){ HolderName = "Joseph Richards", Number = 1, CurrentBalance = 100 },
+                   new BankAccount(){ HolderName = "Joseph Urso", Number = 1, CurrentBalance = 100 },
+                };
 
-                BankAccount bankAccount = new BankAccount();
+                for (int i = 0; i < accounts.Length; i++)
+                {
 
-                Console.Write("Enter account holder name: ");
-                bankAccount.HolderName = Console.ReadLine();
-                Console.Write("Enter account number: ");
+                    Console.WriteLine($"{i + 1}. {accounts[i].HolderName}");
+                }
 
-                // when parsing strings, space or special characters
-                bankAccount.Number = int.Parse(Console.ReadLine());
-                Console.Write("Enter account current balance: ");
-                bankAccount.CurrentBalance = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("\nNew bank account details:");
-                Console.WriteLine("Holder name: " + bankAccount.HolderName);
-                Console.WriteLine("Account number: " + bankAccount.Number);
-                Console.WriteLine("Current balance: " + bankAccount.CurrentBalance);
-
+                int selectedNumber;
+                Console.Write("Enter holder index number: ");
+                selectedNumber = int.Parse(Console.ReadLine());
+                selectedNumber--;
+                if (selectedNumber <= 0 || selectedNumber >= accounts.Length) {
+                    Console.WriteLine("Invalid serial number");
+                }
+                else
+                {
+                    Console.WriteLine(accounts[selectedNumber].HolderName);
+                }
             }
-            catch (FormatException ex)
+            catch (IndexOutOfRangeException ex)
             {
-
                 Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.StackTrace);
             }
+
 
             Console.ReadKey();
         }
---
+++ 29 - Exception Handling/NullReferenceExceptionExample/NullReferenceExceptionExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullReferenceExceptionExample
+{
+    class BankAccount
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public double CurrentBalance { get; set; }
+    
+    }
+
+    class FundasTranfer
+    {
+        public void Transfer(BankAccount sourceAccount, BankAccount destinationAccount, double amount)
+        {
+            try
+            {
+                sourceAccount.CurrentBalance -= amount;
+                destinationAccount.CurrentBalance += amount;
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Reference is null");
+                throw;
+            }
+           
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                BankAccount account1 = new BankAccount() { AccountNumber = 1, AccountHolderName = "Bob", CurrentBalance = 10000 };
+                BankAccount account2 = new BankAccount() { AccountNumber = 2, AccountHolderName = "Jose", CurrentBalance = 40000 };
+                // will result in a NullReferenceException
+                BankAccount account3 = null;
+
+
+                FundasTranfer fundsTranfers = new FundasTranfer();
+                fundsTranfers.Transfer(account2, account1, 500);
+
+                fundsTranfers.Transfer(account3, account1, 90);
+
+                Console.WriteLine(account1.AccountHolderName + ", " + account1.CurrentBalance);
+                Console.WriteLine(account2.AccountHolderName + ", " + account2.CurrentBalance);
+            }
+            catch (NullReferenceException exc)
+            {
+
+                Console.WriteLine(exc.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
