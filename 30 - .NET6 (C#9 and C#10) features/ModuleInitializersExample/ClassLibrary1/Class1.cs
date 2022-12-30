07e96833b914c359c456d82075401879c977e3fd
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-30 09:59:05 -0500

    add class 291 - Module Initializers

---
+++ 30 - .NET6 (C#9 and C#10) features/ModuleInitializersExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+public class ClassInitializer
+{
+    [ModuleInitializer]
+    internal static void Init2()
+    {
+        Console.WriteLine("Class initializer");
+    }
+}
---
+++ 30 - .NET6 (C#9 and C#10) features/ModuleInitializersExample/ModuleInitializersExample/Initializer1.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Initializer1
+{
+    // can create a single initializer file
+    // which calls other methods
+
+
+    [ModuleInitializer]
+    internal static void Init2()
+    {
+        Console.WriteLine("Init 2 executed");
+    }
+
+    [ModuleInitializer]
+    internal static void Init1()
+    {
+        Console.WriteLine("Init 1 executed");
+    }
+
+}
