256cd7bddce88df7986b618eb2bdc265eb6774ea
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-30 10:22:47 -0500

    add class 292 - Nullable Reference Types

---
+++ 30 - .NET6 (C#9 and C#10) features/NullReferenceTypesExample/NullReferenceTypesExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+
+class Employee
+{
+    public string Name { get; set; }
+}
+
+class EmployeeBusinessLogic
+{
+    public Employee? GetEmployee()
+    {
+        //return new Employee() { Name = "Joseph" };
+        return null;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
+        Employee? employee = employeeBusinessLogic.GetEmployee();
+
+        Console.WriteLine(employee?.Name);
+
+        // or perform conditional
+        if (employee != null)
+        {
+            Console.WriteLine(employee.Name);
+        }
+    }
+}
