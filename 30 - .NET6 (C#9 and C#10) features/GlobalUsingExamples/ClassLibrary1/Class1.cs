8683163efd2af2f3d96069b916c7ca704268c843
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-30 09:35:53 -0500

    add class 290 - Global Using

---
+++ 30 - .NET6 (C#9 and C#10) features/GlobalUsingExamples/ClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        List<string> strings = new List<string>() { "fds", "xx", "ddd" };
+
+    }
+}
---
+++ 30 - .NET6 (C#9 and C#10) features/GlobalUsingExamples/GlobalUsingExamples/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// dont need to import in the same file
+// good practice to have GlobalUsings file to each project
+
+List<string> strings = new List<string>() { "fds", "xx", "ddd" };
+
+Console.WriteLine(strings[1]);
