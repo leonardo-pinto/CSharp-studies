ebb77ed568c5b5b94926060fd85dd83668234a52
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-31 09:37:39 -0500

    add class 300 - Relational and Logical Pattern

--- 30 - .NET6 (C#9 and C#10) features/PatternMatchingExample/PatternMatchingExample/Program.cs
+++ 30 - .NET6 (C#9 and C#10) features/PatternMatchingExample/PatternMatchingExample/Program.cs
@@ -59,13 +59,27 @@ class Descripter
         //        return $"{person.Name}, {person.Age}";
         //}
         // switch expression to improve
+        //return person switch
+        //{
+        //    Manager man when (man.Salary > 1000) => $"{person.Name}, {person.Age}, {man.Salary}",
+        //    Employee emp => $"{person.Name}, {person.Age}, {emp.Salary}",
+        //    Supplier sup when (sup.SupplierBalance > 10000) => $"{person.Name}, {person.Age}, {sup.SupplierBalance}",
+        //    Customer cus => $"{person.Name}, {person.Age}, {cus.CustomerBalance}",
+        //    _ => $"{person.Name}, {person.Age}",
+        //};
+
         return person switch
         {
-            Manager man when (man.Salary > 1000) => $"{person.Name}, {person.Age}, {man.Salary}",
-            Employee emp => $"{person.Name}, {person.Age}, {emp.Salary}",
-            Supplier sup when (sup.SupplierBalance > 10000) => $"{person.Name}, {person.Age}, {sup.SupplierBalance}",
-            Customer cus => $"{person.Name}, {person.Age}, {cus.CustomerBalance}",
-            _ => $"{person.Name}, {person.Age}",
+            Person p when p.Age is <= 13 => $"{person.Name} is a child",
+            // p.Age <= 13
+            Person p when p.Age is > 13 and <= 18 => $"{person.Name} is a teenager",
+            // p.Age > 13 && p.Age <= 18
+            Person p when p.Age is > 18 and < 60 => $"{person.Name} is an adult",
+            // p.Age > 18 && p.Age < 60
+            Person p when p.Age is >= 60 and < 100 => $"{person.Name} is a senior",
+            // p.Age >= 60
+            Person p when p.Age is 100 or 200 => $"{person.Name} is a centenarian",
+            _ => $"{person.Name} is {person.Age} years old",
         };
     }
 }
@@ -74,10 +88,14 @@ class Program
 {
     static void Main()
     {
-        Manager manager = new() { Name = "John", Age = 45, Salary = 1500 };
-        Supplier supplier = new() { Name = "John", Age = 45, SupplierBalance = 120000 };
+        Manager manager = new() { Name = "Leonardo", Age = 6, Salary = 1500 };
+        Supplier supplier = new() { Name = "Enzo", Age = 17, SupplierBalance = 120000 };
+        Employee emp = new() { Name = "John Doe", Age = 56 };
+        Employee emp2 = new() { Name = "Joseph Richards", Age = 100 };
         Console.WriteLine(Descripter.GetDescription(manager));
         Console.WriteLine(Descripter.GetDescription(supplier));
+        Console.WriteLine(Descripter.GetDescription(emp));
+        Console.WriteLine(Descripter.GetDescription(emp2));
 
         Console.ReadKey();
     }
