b0a9c9ca1015c153e4d6ffa8cbcf3e612283d88d
leonardo-pinto <leonardoan.pinto@gmail.com>
2022-12-31 09:57:53 -0500

    add class 301 - Property Pattern

--- 30 - .NET6 (C#9 and C#10) features/PatternMatchingExample/PatternMatchingExample/Program.cs
+++ 30 - .NET6 (C#9 and C#10) features/PatternMatchingExample/PatternMatchingExample/Program.cs
@@ -1,8 +1,14 @@
-﻿class Person
+﻿enum MaritalStatus
+{
+    Single, Married
+}
+
+class Person
 {
     // allow null values
     public string? Name { get; set; }
     public int? Age { get; set; }
+    public MaritalStatus PersonMaritalStatus { get; set; }
 }
 
 class Employee : Person
@@ -68,17 +74,37 @@ class Descripter
         //    _ => $"{person.Name}, {person.Age}",
         //};
 
+        // Relational and Logical pattern
+        //return person switch
+        //{
+        //    Person p when p.Age is <= 13 => $"{person.Name} is a child",
+        //    // p.Age <= 13
+        //    Person p when p.Age is > 13 and <= 18 => $"{person.Name} is a teenager",
+        //    // p.Age > 13 && p.Age <= 18
+        //    Person p when p.Age is > 18 and < 60 => $"{person.Name} is an adult",
+        //    // p.Age > 18 && p.Age < 60
+        //    Person p when p.Age is >= 60 and < 100 => $"{person.Name} is a senior",
+        //    // p.Age >= 60
+        //    Person p when p.Age is 100 or 200 => $"{person.Name} is a centenarian",
+        //    _ => $"{person.Name} is {person.Age} years old",
+        //};
+
+        // Property Pattern example
         return person switch
         {
-            Person p when p.Age is <= 13 => $"{person.Name} is a child",
-            // p.Age <= 13
-            Person p when p.Age is > 13 and <= 18 => $"{person.Name} is a teenager",
-            // p.Age > 13 && p.Age <= 18
-            Person p when p.Age is > 18 and < 60 => $"{person.Name} is an adult",
-            // p.Age > 18 && p.Age < 60
-            Person p when p.Age is >= 60 and < 100 => $"{person.Name} is a senior",
-            // p.Age >= 60
-            Person p when p.Age is 100 or 200 => $"{person.Name} is a centenarian",
+            // first tries to cast type, then checks conditions
+            // then assigns p value and return
+            { Age: <= 13 } p => $"{person.Name} is a child",
+            //Person p when p.Age is <= 13 => $"{person.Name} is a child",
+           
+            Person { Age: > 13 and <= 18 } p => $"{person.Name} is a teenager",
+           
+            Person { Age: > 18 and < 60, PersonMaritalStatus: MaritalStatus.Single } p => $"{person.Name} is a happy adult",
+            Person { Age: > 18 and < 60, PersonMaritalStatus: MaritalStatus.Married } p => $"{person.Name} is a sad adult",
+
+            Person { Age: >= 60 and < 100 } p => $"{person.Name} is a senior",
+         
+            Person { Age: 100 or 200 } p => $"{person.Name} is a centenarian",
             _ => $"{person.Name} is {person.Age} years old",
         };
     }
@@ -88,10 +114,10 @@ class Program
 {
     static void Main()
     {
-        Manager manager = new() { Name = "Leonardo", Age = 6, Salary = 1500 };
-        Supplier supplier = new() { Name = "Enzo", Age = 17, SupplierBalance = 120000 };
-        Employee emp = new() { Name = "John Doe", Age = 56 };
-        Employee emp2 = new() { Name = "Joseph Richards", Age = 100 };
+        Manager manager = new() { Name = "Leonardo", Age = 6, Salary = 1500, PersonMaritalStatus = MaritalStatus.Married };
+        Supplier supplier = new() { Name = "Enzo", Age = 20, SupplierBalance = 120000, PersonMaritalStatus = MaritalStatus.Single };
+        Employee emp = new() { Name = "John Doe", Age = 35, PersonMaritalStatus = MaritalStatus.Married };
+        Employee emp2 = new() { Name = "Joseph Richards", Age = 100, PersonMaritalStatus = MaritalStatus.Married };
         Console.WriteLine(Descripter.GetDescription(manager));
         Console.WriteLine(Descripter.GetDescription(supplier));
         Console.WriteLine(Descripter.GetDescription(emp));
