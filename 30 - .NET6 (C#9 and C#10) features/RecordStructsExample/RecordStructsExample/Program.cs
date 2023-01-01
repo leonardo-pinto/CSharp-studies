463d033cc35c9a782eaf4c8581934f51aab69004
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-01 11:33:31 -0500

    add class 320 - Record structs

---
+++ 30 - .NET6 (C#9 and C#10) features/RecordStructsExample/RecordStructsExample/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace record_structs_example
+{
+    // without readonly, all properties are by default read/write
+    public readonly record struct Person(string? Name, int? Age);
+
+    public record struct Employee(string? Name, int? Age);
+
+    class Program
+    {
+        static void Main()
+        {
+            Person person1 = new("Joseph", 50);
+            Person person2 = person1; // reference copy
+
+
+            Employee employee1 = new("Dawkings", 29);
+            Employee employee2 = employee1;
+            employee1.Age = 35;
+            // Structs are value type
+            Console.WriteLine(employee1.ToString());
+            Console.WriteLine(employee2.ToString());
+            //Console.WriteLine(person1.ToString());
+        }
+    }
+}
