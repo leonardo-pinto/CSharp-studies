14b36a382ca75520d4e38dd40c9e1b2040804abc
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-01 11:52:51 -0500

    add class 322 - Command Line Arguments Part 2

--- 30 - .NET6 (C#9 and C#10) features/CommandLineExample/CommandLineExample/Program.cs
+++ 30 - .NET6 (C#9 and C#10) features/CommandLineExample/CommandLineExample/Program.cs
@@ -1,7 +1,11 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine(args.Length);
-    }
-}
+﻿// Part 1
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine(args.Length);
+//    }
+//}
+
+// Part 2
+Console.WriteLine(args.Length);
