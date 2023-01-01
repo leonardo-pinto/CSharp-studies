9432afcd18884eef620aa7ba7348cf184e7ea67c
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-01 17:44:49 -0500

    add class 327 - Interface Default Methods

---
+++ 30 - .NET6 (C#9 and C#10) features/InterfaceDefaultMethodsExample/InterfaceDefaultMethodsExample/Program.cs
@@ -0,0 +1,40 @@
+﻿public interface IEmployee
+{
+    public string Name { get; set; }
+
+    public string GetNameInUpperCase()
+    {
+        return Name.ToUpper();
+    }
+}
+
+public class Manager: IEmployee
+{
+    private string? _name;
+
+    public string? Name { get; set; }
+}
+
+public class Clerk: IEmployee
+{
+    private string? _name;
+
+    public string? Name { set; get; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Manager manager = new();
+        manager.Name = "Josias";
+        Clerk clerk = new();
+        clerk.Name = "John";
+
+        IEmployee iEmpManager = (IEmployee)manager;
+        IEmployee iEmpClerk = (IEmployee)clerk;
+
+        Console.WriteLine(iEmpManager.GetNameInUpperCase());
+        Console.WriteLine(iEmpClerk.GetNameInUpperCase());
+    }
+}
