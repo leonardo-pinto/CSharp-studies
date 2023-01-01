78bbdf564361c2acf87f30c8fbaa6239987c4e73
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-01 17:27:41 -0500

    add class 325 - Lambda Function Return Type

---
+++ 30 - .NET6 (C#9 and C#10) features/LambdaExpressionsExample/LambdaExpressionsExample/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var getMessage = () => "Hello world";
+        // scenario 1
+        // alternative Func<int, object> getResult = (marks) => //..
+        var getResult = object (int marks) =>
+        {
+            if (marks >= 35)
+            {
+                return "Pass";
+            }
+            else
+            {
+                return 0;
+            }
+        };
+
+        Console.WriteLine(getResult(40));
+
+        // scenario 2
+        var GetNumbers = IList<int>() => new int[] { 1, 2, 3 };
+
+        foreach(int number in GetNumbers())
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
