2cb0390c0f5387c2166ee8fa972ab650acf0f3cb
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-10 17:32:54 -0500

    add task 4.3

--- ASP.NET/03 - HTTP/Task4/Task4/Program.cs
+++ ASP.NET/03 - HTTP/Task4/Task4/Program.cs
@@ -15,11 +15,45 @@ app.Run(async (HttpContext context) =>
         // Request Method: GET
         // Response Status Code: 200
         // Response Body(output): 7
+        //if (
+        //    context.Request.Query.ContainsKey("firstNumber")
+        //    && context.Request.Query.ContainsKey("secondNumber")
+        //    && context.Request.Query.ContainsKey("operation")
+        //)
+        //{
+        //    double firstNumber = int.Parse(context.Request.Query["firstNumber"]);
+        //    double secondNumber = int.Parse(context.Request.Query["secondNumber"]);
+        //    double result = 0;
+        //    switch (context.Request.Query["operation"])
+        //    {
+        //        case "add":
+        //            result = firstNumber + secondNumber;
+        //            break;
+        //        default:
+        //            break;
+        //    }
+
+        //    await context.Response.WriteAsync($"Result is {result}");
+        //}
+
+        //        Example #3:
+
+        //If you receive a HTTP GET request at path "/", if firstNumber,
+        //secondNumber and operator are submitted, if "operation" is incorrect,
+        //it should return HTTP 200 response.
+
+        //Request Url: /? firstNumber = 5 & secondNumber = 2 & operation = something
+
+        //Request Method: GET
+
+        //Response Status Code: 400
+
+        //Response Body(output): Invalid input for 'operation'
         if (
-            context.Request.Query.ContainsKey("firstNumber")
-            && context.Request.Query.ContainsKey("secondNumber")
-            && context.Request.Query.ContainsKey("operation")
-        )
+           context.Request.Query.ContainsKey("firstNumber")
+           && context.Request.Query.ContainsKey("secondNumber")
+           && context.Request.Query.ContainsKey("operation")
+       )
         {
             double firstNumber = int.Parse(context.Request.Query["firstNumber"]);
             double secondNumber = int.Parse(context.Request.Query["secondNumber"]);
@@ -29,11 +63,23 @@ app.Run(async (HttpContext context) =>
                 case "add":
                     result = firstNumber + secondNumber;
                     break;
+                case "multiply":
+                    result = firstNumber * secondNumber;
+                    break;
                 default:
                     break;
             }
+            if (result != 0)
+            {
+                context.Response.StatusCode = 200;
+                await context.Response.WriteAsync($"Result is {result}");
+            } else
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid input for operation!");
 
-            await context.Response.WriteAsync($"Result is {result}");
+            }
+
         }
     }
 });
