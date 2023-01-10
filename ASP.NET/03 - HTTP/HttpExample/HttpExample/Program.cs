b384b29de62e97e8a36da8f523adc61e7ce8981b
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-10 16:11:32 -0500

    add class 16 - Query String

--- ASP.NET/03 - HTTP/HttpExample/HttpExample/Program.cs
+++ ASP.NET/03 - HTTP/HttpExample/HttpExample/Program.cs
@@ -5,11 +5,17 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    string requestPath = context.Request.Path;
-    context.Response.Headers["MyKey"] = "my value";
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Query.ContainsKey("id"))
+    {
+        string id = context.Request.Query["id"];
+        await context.Response.WriteAsync($"<h3>Id is {id}<h3/>");
+    }
+    //string requestPath = context.Request.Path;
+    //context.Response.Headers["MyKey"] = "my value";
     context.Response.StatusCode = 200;
-    await context.Response.WriteAsync("<h1>Testing</h1> statusCode");
-    await context.Response.WriteAsync($"Request path: <h2>{requestPath}</>");
+    await context.Response.WriteAsync("\n<h1>Testing</h1> statusCode");
+    //await context.Response.WriteAsync($"Request path: <h2>{requestPath}</>");
 
 });
 
