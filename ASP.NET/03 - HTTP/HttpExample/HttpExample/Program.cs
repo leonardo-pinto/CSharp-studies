1f1b96a04c91a936ec0c912f0a5e6b5cda0c8ca0
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-10 15:58:14 -0500

    add class 15 - Http Request

--- ASP.NET/03 - HTTP/HttpExample/HttpExample/Program.cs
+++ ASP.NET/03 - HTTP/HttpExample/HttpExample/Program.cs
@@ -1,11 +1,16 @@
+using System.Security.Cryptography;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
+    string requestPath = context.Request.Path;
     context.Response.Headers["MyKey"] = "my value";
     context.Response.StatusCode = 200;
     await context.Response.WriteAsync("<h1>Testing</h1> statusCode");
+    await context.Response.WriteAsync($"Request path: <h2>{requestPath}</>");
+
 });
 
 app.Run();
