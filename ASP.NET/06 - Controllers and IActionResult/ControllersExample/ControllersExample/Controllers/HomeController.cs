b1d89b4ad2595e7458578d0388f87f6eefed8a2d
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-17 16:05:39 -0500

    add class 47 - ContentResult

--- ASP.NET/06 - Controllers and IActionResult/ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ASP.NET/06 - Controllers and IActionResult/ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -2,13 +2,18 @@
 
 namespace ControllersExample.Controllers
 {
+    
     // suffix Controller identifies a class as a controller
-   public class HomeController
+    // [Controller] also indicates a controller 
+    public class HomeController : Controller
    {
         [Route("/")]
-        public string Index()
+        public ContentResult Index()
         {
-            return "Hello from Index";
+            //return new ContentResult() { Content = "Hello from Index", ContentType = "text/plain" };
+            // alternative to the above by inheriting Controller class
+            //return Content("Hello from Index", "text/plain");
+            return Content("<h1>This is HTML</h1>", "text/html");
         }
 
         [Route("/about")]
