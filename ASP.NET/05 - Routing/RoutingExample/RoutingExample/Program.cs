77c82bbab60f1f95f8166ff24de7290ed565cea7
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-17 09:21:00 -0500

    add class 33 - GetEndpoints

--- ASP.NET/05 - Routing/RoutingExample/RoutingExample/Program.cs
+++ ASP.NET/05 - Routing/RoutingExample/RoutingExample/Program.cs
@@ -1,15 +1,40 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    Microsoft.AspNetCore.Http.Endpoint endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($" Endpoint: {endpoint.DisplayName}");
+    }
+    await next(context);
+});
+
 // enable routing
 app.UseRouting();
+
+app.Use(async (HttpContext context, RequestDelegate next) =>
+{
+    Microsoft.AspNetCore.Http.Endpoint? endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($" Endpoint: {endpoint.DisplayName}");
+    }
+    await next(context);
+});
 
 app.UseEndpoints(endpoints =>
 {
     // add endpoints
-    endpoints.Map("/map1", async (context) =>
+    //endpoints.Map("/map1", async (context) =>
+    //{
+    //    await context.Response.WriteAsync("In map 1");
+    //});
+
+    endpoints.MapGet("map1", async (context) =>
     {
-        await context.Response.WriteAsync("In map 1");
+        await context.Response.WriteAsync("Map1 Get");
     });
 });
 
