2f156b8c8e08a230af13319d34f4244510493071
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-17 15:50:22 -0500

    add class 45 - Multiple Action Methods

--- ASP.NET/06 - Controllers and IActionResult/ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ASP.NET/06 - Controllers and IActionResult/ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -4,17 +4,23 @@ namespace ControllersExample.Controllers
 {
     // suffix Controller identifies a class as a controller
    public class HomeController
-    {
+   {
         [Route("/")]
-        public string method1()
+        public string Index()
         {
-            return "Hello from method 1";
+            return "Hello from Index";
         }
 
-        [Route("/hello")]
-        public string method2()
+        [Route("/about")]
+        public string About()
         {
-            return "Hello from method 2";
+            return "Hello from About";
+        }
+
+        [Route("/contact")]
+        public string Contact()
+        {
+            return "Hello from Contact";
         }
     }
 }
