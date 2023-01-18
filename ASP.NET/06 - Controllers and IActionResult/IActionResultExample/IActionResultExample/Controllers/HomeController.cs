37112fa7823545c39ca3889c036228df705215a0
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-18 09:10:16 -0500

    add class 51 - Status Code Results

--- ASP.NET/06 - Controllers and IActionResult/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
+++ ASP.NET/06 - Controllers and IActionResult/IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -11,15 +11,13 @@ namespace IActionResultExample.Controllers
             // Book id should be applied
             if (!Request.Query.ContainsKey("bookId"))
             {
-                Response.StatusCode = 400;
-                return Content("Book id is not supplied");
+                return BadRequest("Book id is not supplied");
             }
             
             // Book id can`t be empty
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookId"])))
             {
-                Response.StatusCode = 400;
-                return Content("Book id can`t be null or empty");
+                return BadRequest("Book id can`t be null or empty");
             }
 
             // Book id should be between 1 to 1000
@@ -27,25 +25,23 @@ namespace IActionResultExample.Controllers
         
             if (bookId <= 0)
             {
-                Response.StatusCode = 400;
-                return Content("Book id can`t be less then or equal to zero");
+               return NotFound("Book id can`t be less then or equal to zero");
             }
             if (bookId > 1000)
             {
-                Response.StatusCode = 400;
-                return Content("Book id can`t be greater than 1000");
+               return NotFound("Book id can`t be greater than 1000");
             }
-
+            System.Console.WriteLine($"loggedIn {Request.Query["isLoggedIn"]}");
             // isLoggedIn should be true
             if (Convert.ToBoolean(Request.Query["isLoggedIn"]) == false)
             {
-                Response.StatusCode = 401;
-                return Content("User must be authenticated");
+                
+                return Unauthorized("User must be authenticated");
             }
 
             // Here we have a problem declaring the method return type
             // ContentResult x FileResult
-            return File("/example2.png", "content/png");
+            return File("/example2.png", "application/png");
         }
     }
 }
