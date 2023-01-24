8aeed4794ef48b3531a39236fc6f43b63e3e6949
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-24 16:43:29 -0500

    add task 10

---
+++ ASP.NET/06 - Controllers and IActionResult/Task10/Task10/Controllers/BankController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Task10.Controllers
+{
+    public class BankController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Content("<h1>Welcome to the Best Bank</h1>", "text/html");
+        }
+
+        [Route("/account-details")]
+        public IActionResult AccountDetails()
+        {
+            return new JsonResult(
+                new
+                {
+                    accountNumber = 1001,
+                    accountHolderName = "John Doe",
+                    currentBalance = 5000
+                }
+            );
+        }
+
+        [Route("/get-current-balance/{accountNumber}")]
+        public IActionResult CurrentBalance()
+        {
+            int accountNumber = Convert.ToInt32(Request.RouteValues["accountNumber"]);
+            if (accountNumber == 1001)
+            {
+                return Content("5000");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+
+}
