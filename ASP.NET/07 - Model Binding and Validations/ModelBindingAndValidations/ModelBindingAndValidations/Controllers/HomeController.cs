6633b77961ca79996352dfa7a5cc0154dd465ccb
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-24 17:15:12 -0500

    add class 56 - Query String vs Route Data

---
+++ ASP.NET/07 - Model Binding and Validations/ModelBindingAndValidations/ModelBindingAndValidations/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingAndValidations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("bookstore/{bookid?}/{isLoggedIn?}")]
+        // Example Model Binding
+        // Priority
+        //
+        //
+        // Route Data
+        // Query String
+        // method parameter is a shortcut
+        // to Request.Query
+        public IActionResult Index(int? bookid, bool? isLoggedIn)
+        {
+            if (bookid.HasValue == false)
+            {
+                return BadRequest("Book is was not supplied");
+            }
+            if (bookid <= 0)
+            {
+                return BadRequest("Book id can not be less than or equal to zero");
+            }
+            if (isLoggedIn.HasValue == false || isLoggedIn == false)
+            {
+                return StatusCode(401);
+            }
+
+            return Content($"<h1>Bookstore {bookid}</h1>", "text/html");
+        }
+    }
+}
