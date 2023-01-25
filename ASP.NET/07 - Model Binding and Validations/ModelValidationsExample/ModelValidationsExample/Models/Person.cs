f2cc2896bcd450eeda4aa7fde385c4efb43a152a
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-25 14:18:55 -0500

    add class 63 - All Model Validations - Part 2

--- ASP.NET/07 - Model Binding and Validations/ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ ASP.NET/07 - Model Binding and Validations/ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -9,11 +9,23 @@ namespace ModelValidationsExample.Models
         [Required(ErrorMessage = "{0} can't be empty or null")]
         [Display(Name = "Person name")]
         [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
+        //[RegularExpression("^[A-Za-z .]$", ErrorMessage = "{0} should contain only alphabets, space and dot(.)")]
         public string? PersonName { get; set; }
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [EmailAddress(ErrorMessage = "{0} should be a proper email address")]
         public string? Email { get; set; }
+       
         public string? Phone { get; set; }
         [Range(0, 120, ErrorMessage = "{0} should be between {1} and {2}")]
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "{0} can't be blank")]
+        [Compare("Password", ErrorMessage = "{0} and {1} don't match")]
+        [Display(Name = "Password confirmation")]
+        public string? ConfirmPassword { get; set; }
 
         public override string ToString()
         {
