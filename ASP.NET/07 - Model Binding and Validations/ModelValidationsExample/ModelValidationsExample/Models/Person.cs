b89edeeec6acffc16c79906ec833f76ba7f95886
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-01-25 14:06:39 -0500

    add class 62 - All Model Validations - Part 1

--- ASP.NET/07 - Model Binding and Validations/ModelValidationsExample/ModelValidationsExample/Models/Person.cs
+++ ASP.NET/07 - Model Binding and Validations/ModelValidationsExample/ModelValidationsExample/Models/Person.cs
@@ -6,10 +6,14 @@ namespace ModelValidationsExample.Models
     {
         // Model validation
         // may be configured using the ErrorMessage attribute
-        [Required]
+        [Required(ErrorMessage = "{0} can't be empty or null")]
+        [Display(Name = "Person name")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long")]
         public string? PersonName { get; set; }
         public string? Email { get; set; }
         public string? Phone { get; set; }
+        [Range(0, 120, ErrorMessage = "{0} should be between {1} and {2}")]
+        public int Age { get; set; }
 
         public override string ToString()
         {
