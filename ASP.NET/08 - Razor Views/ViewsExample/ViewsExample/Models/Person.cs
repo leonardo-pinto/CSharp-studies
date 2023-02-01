9e44913721148c96183183a5a4a59f99de18415c
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-01 08:57:11 -0500

    add class 83 - Local Functions

--- ASP.NET/08 - Razor Views/ViewsExample/ViewsExample/Models/Person.cs
+++ ASP.NET/08 - Razor Views/ViewsExample/ViewsExample/Models/Person.cs
@@ -3,7 +3,7 @@
     public class Person
     {
         public string? Name { get; set; }
-        public int? Age { get; set; }
+        public int Age { get; set; }
         public Gender? Gender { get; set; } 
     }
 
