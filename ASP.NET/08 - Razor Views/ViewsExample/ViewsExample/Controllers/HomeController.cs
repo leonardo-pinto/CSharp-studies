308f95bf8d5688a54e178a67768c15f5126edcb3
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-01 09:09:01 -0500

    add class 85 - ViewData - Part 1

--- ASP.NET/08 - Razor Views/ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ASP.NET/08 - Razor Views/ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
@@ -7,6 +8,20 @@ namespace ViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            // Controller should supply all the data to View
+            ViewData["appTitle"] = "* ASP.NET Core Demo App *";
+            //string appTitle = "ASP.NET Core Demo App";
+
+            List<Person> people = new List<Person>()
+            {
+                new Person() { Name = "John Doe", Age=40, Gender=Gender.Male },
+                new Person() { Name = "Jane Doe", Age=30, Gender=Gender.Female },
+                new Person() { Name = "Jimmy Doe", Age=15, Gender=Gender.Male },
+
+            };
+
+            ViewData["people"] = people;
+
             return View(); // /Views/Home/Index.cshtml
         }
     }
