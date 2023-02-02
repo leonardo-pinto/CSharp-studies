cd9dfba81e919357f7810916a2a2502895546bee
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-02 11:17:49 -0500

    add class 89 - Strongly Typed Views - Part 2

--- ASP.NET/08 - Razor Views/ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ASP.NET/08 - Razor Views/ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -14,15 +14,37 @@ namespace ViewsExample.Controllers
 
             List<Person> people = new List<Person>()
             {
-                new Person() { Name = "John Doe", Age=40, Gender=Gender.Male },
-                new Person() { Name = "Jane Doe", Age=30, Gender=Gender.Female },
-                new Person() { Name = "Jimmy Doe", Age=15, Gender=Gender.Male },
+                new Person() { Name = "john", Age=40, Gender=Gender.Male },
+                new Person() { Name = "jane", Age=30, Gender=Gender.Female },
+                new Person() { Name = "jimmy", Age=15, Gender=Gender.Male },
             };
 
             //ViewData["people"] = people;
             //ViewBag.people = people;
 
             return View(people); // /Views/Home/Index.cshtml
+        }
+
+        [Route("/person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if (name == null)
+            {
+                return Content("Person name can't be null");
+            }
+            else
+            {
+                List<Person> people = new List<Person>()
+                {
+                    new Person() { Name = "john", Age=40, Gender=Gender.Male },
+                    new Person() { Name = "jane", Age=30, Gender=Gender.Female },
+                    new Person() { Name = "jimmy", Age=15, Gender=Gender.Male },
+                };
+
+                Person? matchingPerson = people.Where(person => person.Name == name).FirstOrDefault();
+                // Should add View first argument if dont matches the method name
+                return View(matchingPerson);
+            }
         }
     }
 }
