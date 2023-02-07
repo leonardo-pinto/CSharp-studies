82eca489b49ffec1edc6910377899e4b2049f931
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-07 08:34:26 -0500

    add class Creating Layout Views - Part 1

---
+++ ASP.NET/09 - Layout Views/LayoutViewsExample/LayoutViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("products/search")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [Route("products/order")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
