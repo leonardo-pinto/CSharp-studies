8e5b7befb2c9bb435fd5f3806ebfe7673e72d272
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-08 08:58:35 -0500

    add class 110 - View Components with ViewData

--- ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample.ViewComponents
 {
@@ -10,6 +11,17 @@ namespace ViewComponentsExample.ViewComponents
         // method must be public and async
         public async Task<IViewComponentResult> InvokeAsync()
         {
+            PersonGridModel model = new()
+            {
+                GridTitle = "Persons List",
+                Persons = new List<Person>() {
+                    new Person(){ Id = 1, Name = "John Doe" },
+                    new Person(){ Id = 2, Name = "Joseph Richards" }
+                }
+            };
+
+            ViewBag.Grid = model;
+
             // pass View name as argument if it is different
             // than Default.cshtml
             return View(); 
