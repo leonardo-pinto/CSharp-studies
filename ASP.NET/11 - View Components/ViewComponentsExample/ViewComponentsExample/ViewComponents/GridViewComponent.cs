aaec40aad0e9ee64dd1e9c506651b1e96457ddec
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-08 08:42:48 -0500

    add class 109 - Creating View Components - Part 2

--- ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -7,8 +7,11 @@ namespace ViewComponentsExample.ViewComponents
     //[ViewComponent]
     public class GridViewComponent : ViewComponent
     {
-        Task<IViewComponentResult> InvokeAsync()
+        // method must be public and async
+        public async Task<IViewComponentResult> InvokeAsync()
         {
+            // pass View name as argument if it is different
+            // than Default.cshtml
             return View(); 
             // invoked a partial view from
             // Views/Shared/Components/Grid/Default.cshtml
