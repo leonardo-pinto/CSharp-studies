5702f2300019e04b499f7d8fa76661fe411519a0
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-08 08:26:37 -0500

    add class 108 - Creating View Components - Part 1

---
+++ ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponentsExample.ViewComponents
+{
+    // Attribute or ViewComponent suffix, 
+    // in which the second is recommended
+    //[ViewComponent]
+    public class GridViewComponent : ViewComponent
+    {
+        Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(); 
+            // invoked a partial view from
+            // Views/Shared/Components/Grid/Default.cshtml
+        }
+    }
+}
