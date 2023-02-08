3dfbe0c35ceae20e67f7a4163c3522c16b93d29c
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-08 09:55:39 -0500

    add class 113 - ViewComponentResult

--- ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/Controllers/HomeController.cs
+++ ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample.Controllers
 {
@@ -14,6 +15,22 @@ namespace ViewComponentsExample.Controllers
         public IActionResult About()
         {
             return View();
+        }
+
+        [Route("load-friends-list")]
+        public IActionResult LoadFriendsList()
+        {
+            PersonGridModel personGridModel = new()
+            {
+                GridTitle = "Persons List",
+                Persons = new List<Person>() {
+                    new Person(){ Id = 1, Name = "John Doe" },
+                    new Person(){ Id = 2, Name = "Joseph Richards" }
+                }
+            };
+
+
+            return ViewComponent("Grid", new { grid = personGridModel });
         }
     }
 }
---
+++ ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/wwwroot/friends.js
@@ -0,0 +1,6 @@
+﻿document.querySelector("#load-friends-button")
+    .addEventListener("click", async () => {
+        var response = await fetch("/load-friends-list", { method: "GET" })
+        var content = await response.text();
+        document.querySelector("#list").innerHTML = content;
+    });
