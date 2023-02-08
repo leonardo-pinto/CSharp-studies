ab0584921a95ec4e39381d9fd0a331e31b11e346
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-08 09:35:06 -0500

    add class 112 - View Components with Parameters

--- ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ ASP.NET/11 - View Components/ViewComponentsExample/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -9,22 +9,22 @@ namespace ViewComponentsExample.ViewComponents
     public class GridViewComponent : ViewComponent
     {
         // method must be public and async
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(PersonGridModel grid)
         {
-            PersonGridModel personGridModel = new()
-            {
-                GridTitle = "Persons List",
-                Persons = new List<Person>() {
-                    new Person(){ Id = 1, Name = "John Doe" },
-                    new Person(){ Id = 2, Name = "Joseph Richards" }
-                }
-            };
+            //PersonGridModel personGridModel = new()
+            //{
+            //    GridTitle = "Persons List",
+            //    Persons = new List<Person>() {
+            //        new Person(){ Id = 1, Name = "John Doe" },
+            //        new Person(){ Id = 2, Name = "Joseph Richards" }
+            //    }
+            //};
 
             //ViewBag.Grid = model;
             // pass View name as argument if it is different
             // than Default.cshtml
             // Example of strongly typed view
-            return View("Default", personGridModel); 
+            return View("Default", grid); 
             // invoked a partial view from
             // Views/Shared/Components/Grid/Default.cshtml
         }
