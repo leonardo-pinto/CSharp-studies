e13948ac437e15e509ca02e5f8ce1fe9468bbadd
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-15 08:37:34 -0500

    add class 118 - Inversion of Control

--- ASP.NET/12 - Dependency Injection/DependencyInjectionExample/DependencyInjectionExample/Program.cs
+++ ASP.NET/12 - Dependency Injection/DependencyInjectionExample/DependencyInjectionExample/Program.cs
@@ -1,5 +1,15 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
+// .Services represents the IOC container
 builder.Services.AddControllersWithViews();
+
+builder.Services.Add(new ServiceDescriptor(
+    typeof(ICitiesService),
+    typeof(CitiesService),
+    ServiceLifetime.Transient
+));
 
 var app = builder.Build();
 app.UseRouting();
