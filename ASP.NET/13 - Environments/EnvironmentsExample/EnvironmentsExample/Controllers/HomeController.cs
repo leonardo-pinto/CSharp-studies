fd9c54e74eaebab0ad45dc62821fab60fff3d952
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-16 14:47:07 -0500

    add class 130 - Environment in Launch Settings

---
+++ ASP.NET/13 - Environments/EnvironmentsExample/EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("same-route")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("same-route")]
+        public IActionResult Other()
+        {
+            return View();
+        }
+    }
+}
