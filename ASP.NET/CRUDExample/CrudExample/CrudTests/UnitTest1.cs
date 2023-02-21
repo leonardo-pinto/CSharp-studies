8b77e3471574d50c03027d589cef0469e2ca7cbc
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-21 17:51:55 -0500

    add class 147 - Add Country - xUnitTest - Part 1

--- ASP.NET/CRUDExample/CrudExample/CrudTests/UnitTest1.cs
+++ ASP.NET/CRUDExample/CrudExample/CrudTests/UnitTest1.cs
@@ -5,17 +5,7 @@ namespace CrudTests
         [Fact]
         public void Test1()
         {
-            // Arrange
-            MyMath myMath = new();
-            int input1 = 10;
-            int input2 = 5;
-            int expected = 15;
-
-            // Act
-            int actual = myMath.Add(input1, input2);
-
-            // Assert
-            Assert.Equal(expected, actual);
+         
         }
     }
 }
---
+++ ASP.NET/CRUDExample/CrudExample/Entities/Country.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities
+{
+    /// <summary>
+    /// Domain Model for country details
+    /// </summary>
+    public class Country
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+}
