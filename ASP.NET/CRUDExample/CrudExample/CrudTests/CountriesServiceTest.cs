7f7de2e0a901ffab1f738f4e6df94becbafcfeaa
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-21 18:05:22 -0500

    add class 148 - Add Country - xUnit Test - Part 2

---
+++ ASP.NET/CRUDExample/CrudExample/CrudTests/CountriesServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using ServiceContracts;
+
+namespace CrudTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+    
+        public CountriesServiceTest()
+        {
+            _countriesService
+        }
+    }
+}
