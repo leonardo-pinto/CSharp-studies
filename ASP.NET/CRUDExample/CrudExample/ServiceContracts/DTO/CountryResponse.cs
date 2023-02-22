df8d9dcf20a473b6086f8ecbc4d14135098cf506
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-22 09:22:13 -0500

    add class 152 - GetAllCountries - Implementation

--- ASP.NET/CRUDExample/CrudExample/ServiceContracts/DTO/CountryResponse.cs
+++ ASP.NET/CRUDExample/CrudExample/ServiceContracts/DTO/CountryResponse.cs
@@ -10,6 +10,27 @@ namespace ServiceContracts.DTO
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
 
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != typeof(CountryResponse))
+            {
+                return false;
+            }
+
+            CountryResponse countryToCompare = (CountryResponse)obj;
+
+            return CountryID == countryToCompare.CountryID && CountryName == countryToCompare.CountryName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 
     // method extension for Country
