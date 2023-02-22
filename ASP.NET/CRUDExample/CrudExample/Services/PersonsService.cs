308c25f981c588f3fd30c82e194858b042f1faed
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-22 11:56:13 -0500

    add class 161 - Get Person By Person ID - Implementation

--- ASP.NET/CRUDExample/CrudExample/Services/PersonsService.cs
+++ ASP.NET/CRUDExample/CrudExample/Services/PersonsService.cs
@@ -47,7 +47,19 @@ namespace Services
 
         public PersonResponse? GetPersonByPersonID(Guid? personID)
         {
-            throw new NotImplementedException();
+            if (personID == null)
+            {
+                return null;
+            }
+
+            Person? person = _persons.FirstOrDefault(person => person.PersonID == personID);
+
+            if (person == null)
+            {
+                return null;
+            }
+
+            return person.ToPersonResponse();
         }
     }
 }
