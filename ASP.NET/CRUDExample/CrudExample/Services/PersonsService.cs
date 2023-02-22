a01933d87900a1144418c4029edf7edca6e7bc28
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-22 13:59:30 -0500

    add class 163 - Get All Persons - Implementation

--- ASP.NET/CRUDExample/CrudExample/Services/PersonsService.cs
+++ ASP.NET/CRUDExample/CrudExample/Services/PersonsService.cs
@@ -42,7 +42,7 @@ namespace Services
 
         public List<PersonResponse> GetAllPersons()
         {
-            throw new NotImplementedException();
+            return _persons.Select(person => person.ToPersonResponse()).ToList();
         }
 
         public PersonResponse? GetPersonByPersonID(Guid? personID)
