561c39029be34b9cdefb3eb0565a067ec11938e1
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-24 09:11:58 -0500

    add class 175 - Gettint Started with UI

---
+++ ASP.NET/CRUDExample/CrudExample/CrudExample/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudExample.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
