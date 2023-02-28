6c88730d1c84f357a35834173dce5d491e62ddd4
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-28 11:47:48 -0500

    add class 189 - Input Tag Helpers - Part 2

--- ASP.NET/CRUDExample/CrudExample/CrudExample/Controllers/PersonsController.cs
+++ ASP.NET/CRUDExample/CrudExample/CrudExample/Controllers/PersonsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
@@ -56,7 +57,8 @@ namespace CrudExample.Controllers
         public IActionResult Create()
         {
             List<CountryResponse> countries = _countriesService.GetAllCountries();
-            ViewBag.Countries = countries;
+            ViewBag.Countries = countries.Select(item => new SelectListItem() { Text = item.CountryName, Value = item.CountryID.ToString() });
+
             return View();
         }
 
