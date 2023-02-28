360fac2be040da7ae3551fa26b4983a57a73bda0
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-02-28 12:08:59 -0500

    add class 190 - Client Side Validations

--- ASP.NET/CRUDExample/CrudExample/ServiceContracts/DTO/PersonAddRequest.cs
+++ ASP.NET/CRUDExample/CrudExample/ServiceContracts/DTO/PersonAddRequest.cs
@@ -15,7 +15,12 @@ namespace ServiceContracts.DTO
         [EmailAddress(ErrorMessage = "Email should be a valid email")]
         public string? Email { get; set; }
         public DateTime? DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Please select a gender")]
+
         public GenderOptions? Gender { get; set; }
+        
+        [Required(ErrorMessage = "Please select a country")]
         public Guid? CountryID { get; set; }
         public string? Address { get; set; }
         public bool ReceiveNewsLetters { get; set; }
