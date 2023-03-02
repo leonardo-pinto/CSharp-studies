6370661ce8ae73a52f03938284ff7f40658356a0
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-02 13:01:42 -0500

    add class 198 - DbContext and DbSet

--- ASP.NET/CRUDExample/CrudExample/CrudExample/Program.cs
+++ ASP.NET/CRUDExample/CrudExample/CrudExample/Program.cs
@@ -1,5 +1,7 @@
 using ServiceContracts;
 using Services;
+using Microsoft.EntityFrameworkCore;
+using Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -7,6 +9,12 @@ builder.Services.AddControllersWithViews();
 // add servicos into IOC
 builder.Services.AddSingleton<ICountriesService, CountriesService>();
 builder.Services.AddSingleton<IPersonsService, PersonsService>();
+
+// DbContext as a service - EFC
+builder.Services.AddDbContext<PersonsDbContext>(options =>
+{
+    options.UseSqlServer();
+});
 
 var app = builder.Build();
 
--- ASP.NET/CRUDExample/CrudExample/Entities/Country.cs
+++ ASP.NET/CRUDExample/CrudExample/Entities/Country.cs
@@ -1,11 +1,23 @@
-﻿namespace Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
 {
     /// <summary>
     /// Domain Model for country details
     /// </summary>
     public class Country
     {
+        // Entity Framework Core
+        [Key] // Primary key
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
     }
+
+    // Without DB
+    //public class Country
+    //{
+    //    public Guid CountryID { get; set; }
+    //    public string? CountryName { get; set; }
+    //}
 }
+
