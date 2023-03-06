03edf5e1c822932902522ce5fbaf2e2c3e09d1f9
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-06 15:22:52 -0500

    add class 218 - Excel to Database Upload - Part 2

---
+++ ASP.NET/CRUDExample/CrudExample/CrudExample/Controllers/CountriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CrudExample.Controllers
+{
+    [Route("[controller]")]
+    public class CountriesController : Controller
+    {
+        [Route("upload-from-excel")]
+        public IActionResult UploadFromExcel()
+        {
+            return View();
+        }
+    }
+}
