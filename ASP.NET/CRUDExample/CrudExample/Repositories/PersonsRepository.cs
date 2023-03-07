08ba455b0a4a3a8c789e289e848578c379437590
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-07 10:53:06 -0500

    add class 229 - Repository Implementation - Part 1

---
+++ ASP.NET/CRUDExample/CrudExample/Repositories/PersonsRepository.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using RepositoryContracts;
+using System.Linq.Expressions;
+
+namespace Repositories
+{
+    public class PersonsRepository : IPersonsRepository
+    {
+        public Task<Person> AddPerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeletePerson(Guid? personID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person?> GetPersonByPersonID(Guid? personID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Person> UpdatePerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
