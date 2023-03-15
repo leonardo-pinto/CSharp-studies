5b3791a9204ee845646d33ffb139e12d46baebb6
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-15 09:07:01 -0400

    add class 285 - Configure Services Extension

--- ASP.NET/CRUDExample/CrudExample/CrudExample/Program.cs
+++ ASP.NET/CRUDExample/CrudExample/CrudExample/Program.cs
@@ -1,50 +1,54 @@
-using ServiceContracts;
-using Services;
-using Microsoft.EntityFrameworkCore;
-using Entities;
-using Repositories;
-using RepositoryContracts;
+//using ServiceContracts;
+//using Services;
+//using Microsoft.EntityFrameworkCore;
+//using Entities;
+//using Repositories;
+//using RepositoryContracts;
 using Serilog;
-using CrudExample.Filters.ActionFilters;
+//using CrudExample.Filters.ActionFilters;
+using CrudExample;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Serilog
-builder.Host.UseSerilog((HostBuilderContext context,
-    IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
-{
-    loggerConfiguration
-    // read configuration settings from built-in IConfiguration
-    .ReadFrom.Configuration(context.Configuration)
-    // read out current app services and make them available to serilog
-    .ReadFrom.Services(services);
-    // must add Serilog to appsettings.json
-});
+//builder.Host.UseSerilog((HostBuilderContext context,
+//    IServiceProvider services, LoggerConfiguration loggerConfiguration) =>
+//{
+//    loggerConfiguration
+//    // read configuration settings from built-in IConfiguration
+//    .ReadFrom.Configuration(context.Configuration)
+//    // read out current app services and make them available to serilog
+//    .ReadFrom.Services(services);
+//    // must add Serilog to appsettings.json
+//});
+
+// calling extension method
+builder.Services.ConfigureServices(builder.Configuration);
 
 // add Global-level filters
-builder.Services.AddControllersWithViews(options => {
+//builder.Services.AddControllersWithViews(options => {
     // can not supply argument values with this syntax
     // options.Filters.Add<ResponseHeaderActionFilter>();
 
     // add ILogger
-    var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
-    options.Filters.Add(new ResponseHeaderActionFilter("My-Key-From-Global", "My-Value-From-Global", 2));
-});
+//    var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<ResponseHeaderActionFilter>>();
+//    options.Filters.Add(new ResponseHeaderActionFilter("My-Key-From-Global", "My-Value-From-Global", 2));
+//});
 
 // add servicos into IOC
-builder.Services.AddScoped<ICountriesService, CountriesService>();
-builder.Services.AddScoped<IPersonsService, PersonsService>();
-builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
-builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
+//builder.Services.AddScoped<ICountriesService, CountriesService>();
+//builder.Services.AddScoped<IPersonsService, PersonsService>();
+//builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+//builder.Services.AddScoped<IPersonsRepository, PersonsRepository>();
 
 // DbContext as a service - EFC
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//{
     // supply connection string
     // should save in configuration
     // scoped service as default ! cannot add into a singleton
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
 
 // String Connection
 // Data Source=(localdb)\MSSQLLocalDB;
