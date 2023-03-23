a39d6bb75f7f780577bf31c33b26f163ad9d1a84
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-23 16:51:29 -0400

    add class 341 - API Versions - Part 3

--- ASP.NET/CitiesManagerSolution/CitiesManager.WebAPI/Program.cs
+++ ASP.NET/CitiesManagerSolution/CitiesManager.WebAPI/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddControllers(options =>
 builder.Services.AddApiVersioning(config =>
 {
     config.ApiVersionReader = new UrlSegmentApiVersionReader();
+
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -24,13 +27,28 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 // Swagger
-
 builder.Services.AddEndpointsApiExplorer(); // enables Swagger to read metadata of endpoints
+
 builder.Services.AddSwaggerGen(options =>
 {
     // Project -> Properties -> Build -> Output -> api.xml
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "api.xml"));
+
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+        { Title = "Cities Web API", Version = "1.0" }
+    );
+
+    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo()
+    { Title = "Cities Web API", Version = "2.0" }
+    );
 }); // configures swagger to generate documentation
+
+//
+builder.Services.AddVersionedApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV"; // enables versioning for two-digits e.g. 1.1
+    options.SubstituteApiVersionInUrl = true;
+});
 
 var app = builder.Build();
 
@@ -39,7 +57,12 @@ app.UseHsts();
 app.UseHttpsRedirection();
 
 app.UseSwagger(); // creates endpointn for swagger.json
-app.UseSwaggerUI(); // creates swagger UI for testing all web api endpoints / action methods
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "1.0");
+    options.SwaggerEndpoint("/swagger/v2/swagger.json", "2.0");
+
+}); // creates swagger UI for testing all web api endpoints / action methods
 
 app.UseAuthorization();
 
