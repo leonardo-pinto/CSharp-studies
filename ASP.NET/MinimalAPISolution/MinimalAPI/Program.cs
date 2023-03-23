dc6c4fee7ddd130cb89a10e15515543a4a6a7023
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-23 17:27:09 -0400

    add class 344 - GET and POST with Minimal API

--- ASP.NET/MinimalAPISolution/MinimalAPI/Program.cs
+++ ASP.NET/MinimalAPISolution/MinimalAPI/Program.cs
@@ -1,13 +1,24 @@
+using MinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", async (HttpContext context) =>
+List<Product> products = new()
 {
-    await context.Response.WriteAsync("GET - Hello World");
+    new Product() { Id = 1, Name = "iPhone" },
+    new Product() { Id = 2, Name = "Dell" }
+};
+
+app.MapGet("/products", async (HttpContext context) =>
+{
+    var content = string.Join("\n", products.Select(temp => temp.ToString()));
+
+    await context.Response.WriteAsync(content);
 });
 
-app.MapPost("/", async (HttpContext context) =>
+app.MapPost("/products", async (HttpContext context, Product product) =>
 {
+    products.Add(product);
     await context.Response.WriteAsync("POST - Hello World");
 });
 
